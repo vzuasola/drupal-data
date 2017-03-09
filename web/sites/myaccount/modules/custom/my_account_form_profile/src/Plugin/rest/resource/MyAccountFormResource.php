ad66ce104d93a082b1c87599861d39160090b1e6
Shamsher Alam <Shamsher.Alam@bayviewtechnology.com>
2017-03-09 14:33:15 +0800

    My Account profile form configuration

---
+++ web/sites/myaccount/modules/custom/my_account_form_profile/src/Plugin/rest/resource/MyAccountFormResource.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\my_account_form_profile\Plugin\rest\resource;
+
+use Drupal\rest\Plugin\ResourceBase;
+use Drupal\rest\ResourceResponse;
+
+/**
+ * Provides a My Account Change Password Resources
+ *
+ * @RestResource(
+ *   id = "change_password_resource",
+ *   label = @Translation("My Account Change Password Resource"),
+ *   uri_paths = {
+ *     "canonical" = "/api/configuration/{id}"
+ *   }
+ * )
+ */
+class MyAccountFormResource extends ResourceBase
+{
+
+    /**
+     * Responds to entity GET requests.
+     * @return \Drupal\rest\ResourceResponse
+     */
+    public function get($id)
+    {
+
+        switch ($id) {
+            case 'my_account_change_password':
+                $config = \Drupal::config('my_account_form_profile.change_password');
+                $values = $config->get();
+                return new ResourceResponse($values);
+                break;
+            case 'my_account_profile':
+                $config = \Drupal::config('my_account_form_profile.profile');
+                $values = $config->get();
+                return new ResourceResponse($values);
+                break;
+            default:
+        }
+    }
+}
