6089b32b49f98aee14a3885f75f024d6a8788c95
Gio Adrian Fortuno <GioAdrian.Fortuno@bayviewtechnology.com>
2017-03-16 16:05:19 +0800

    [DMA-31] : Cashier Module

--- web/sites/myaccount/modules/custom/my_account_cashier/src/Plugin/rest/resource/MyAccountCashierFormResource.php
+++ web/sites/myaccount/modules/custom/my_account_cashier/src/Plugin/rest/resource/MyAccountCashierFormResource.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Drupal\my_account_form_profile\Plugin\rest\resource;
+namespace Drupal\my_account_cashier\Plugin\rest\resource;
 
 use Drupal\rest\Plugin\ResourceBase;
 use Drupal\rest\ResourceResponse;
@@ -9,14 +9,14 @@ use Drupal\rest\ResourceResponse;
  * Provides a My Account Rest Resources
  *
  * @RestResource(
- *   id = "my_account_profile_form_resource",
+ *   id = "my_account_cashier_resource",
  *   label = @Translation("My Account Rest Resource"),
  *   uri_paths = {
  *     "canonical" = "/api/configuration/{id}"
  *   }
  * )
  */
-class MyAccountFormResource extends ResourceBase
+class MyAccountCashierFormResource extends ResourceBase
 {
 
     /**
@@ -25,18 +25,7 @@ class MyAccountFormResource extends ResourceBase
      */
     public function get($id)
     {
-
         switch ($id) {
-            case 'my_account_change_password':
-                $config = \Drupal::config('my_account_form_profile.change_password');
-                $values = $config->get();
-                return new ResourceResponse($values);
-                break;
-            case 'my_account_profile':
-                $config = \Drupal::config('my_account_form_profile.profile');
-                $values = $config->get();
-                return new ResourceResponse($values);
-                break;
             case 'my_account_cashier':
                 $config = \Drupal::config('my_account_cashier.link');
                 $values = $config->get();
