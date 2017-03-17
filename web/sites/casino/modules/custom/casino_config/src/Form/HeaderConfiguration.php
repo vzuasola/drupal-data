08aebac5a7928a326d364fc33a9f522ecd898938
Jyoti Singh <jyoti.singh@bayviewtechnology.com>
2017-03-17 12:02:16 +0800

    CGOII-306-Removing the logo configuration from the settings form.

--- web/sites/casino/modules/custom/casino_config/src/Form/HeaderConfiguration.php
+++ web/sites/casino/modules/custom/casino_config/src/Form/HeaderConfiguration.php
@@ -4,7 +4,6 @@ namespace Drupal\casino_config\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\file\Entity\File;
 
 /**
  * Provides configuration settings form for Header Element Configuration.
@@ -70,30 +69,6 @@ class HeaderConfiguration extends ConfigFormBase {
       '#default_value' => $config->get('login_issue_text'),
       '#required' => TRUE,
     );
-    $form['casino_logo_group'] = array(
-      '#type' => 'details',
-      '#title' => $this->t('Casino Logo Settings'),
-      '#collapsible' => TRUE,
-      '#group' => 'header_settings_tab',
-    );
-    $form['casino_logo_group']['casino_logo'] = array(
-      '#type' => 'managed_file',
-      '#title' => $this->t('Casino Logo Image'),
-      '#description' => t('One file only.Allowed types : png gif jpg jpeg.'),
-      '#default_value' => $config->get('casino_logo'),
-      '#upload_location' => 'public://images/',
-      '#upload_validators'  => array(
-        'file_validate_extensions' => array('png gif jpg jpeg'),
-      ),
-      '#required' => TRUE,
-    );
-    $form['casino_logo_group']['casino_logo_alt'] = array(
-      '#type' => 'textfield',
-      '#title' => $this->t('Logo Alternaive Text'),
-      '#description' => $this->t('This text will be used by screen readers, search engines, or when the image cannot be loaded.'),
-      '#default_value' => $config->get('casino_logo_alt'),
-      '#required' => TRUE,
-    );
 
     return parent::buildForm($form, $form_state);
   }
@@ -106,22 +81,10 @@ class HeaderConfiguration extends ConfigFormBase {
       'join_now_text',
       'join_now_link',
       'login_issue_text',
-      'casino_logo_alt',
-      'casino_logo',
     );
-
-    $icon_field = $form_state->getValue('casino_logo');
-    $file_id = empty($icon_field) ? FALSE : reset($icon_field);
-    if (!empty($file_id)) {
-      // Permanently saving the image files.
-      $file = File::load($file_id);
-      $file->status = FILE_STATUS_PERMANENT;
-      $file->save();
-    }
     foreach ($keys as $key) {
       $this->config('casino_config.header_config')->set($key, $form_state->getValue($key))->save();
     }
-    parent::submitForm($form, $form_state);
   }
 
 }
