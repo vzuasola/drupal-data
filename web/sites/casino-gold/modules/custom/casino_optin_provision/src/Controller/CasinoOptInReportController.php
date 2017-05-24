d420461bbe7f037e7b02b52ea615e9a6f635d5c7
Regie Jose <Regie.Jose@bayviewtechnology.com>
2017-05-24 14:28:16 +0800

    Adding module for casino-gold opt-in provisioning reports

---
+++ web/sites/casino-gold/modules/custom/casino_optin_provision/src/Controller/CasinoOptInReportController.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\casino_optin_provision\Controller\CasinoOptInReportController.
+ */
+
+namespace Drupal\casino_optin_provision\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class CasinoOptInReportController extends ControllerBase {
+
+  /**
+   *
+   */
+  protected $connection;
+
+  /**
+   *
+   */
+  public function __construct($conn){
+    $this->connection = $conn;
+  }
+
+  /**
+   *
+   */
+  public static function create( ContainerInterface $container) {
+    return new static(
+      $container->get('database')
+    );
+  }
+
+  /**
+   *
+   */
+  public function optin_post( Request $request  ) {
+
+    $responseStatus = [
+      'status' => 'failed',
+      'message' => 'Something went wrong, please check your data.',
+    ];
+
+    // This condition checks the `Content-type` and makes sure to
+    // decode JSON string from the request body into array.
+    if ( 0 === strpos( $request->headers->get( 'Content-Type' ), 'application/json' ) ) {
+
+      $data = json_decode( $request->getContent(), TRUE );
+      $request->request->replace( is_array( $data ) ? $data : [] );
+
+      if ( !empty($data['username']) && !empty($data['application_date']) && !empty($data['currency']) ) {
+        try {
+
+          $isOnProcess = $this->checkUsernameIfInProcess($data['username']);
+
+          if ($isOnProcess) {
+            $responseStatus = [
+              'status' => 'on-process',
+              'message' => 'User application is currently on-process',
+            ];
+          } else {
+            $isSaved = db_insert('casino_optin_report')
+                        ->fields(array(
+                          'username' => $data['username'],
+                          'application_date' => $data['application_date'],
+                          'currency' => $data['currency']
+                        ))
+                        ->execute();
+
+            if ($isSaved) {
+              $responseStatus = [
+                'status' => 'success',
+                'message' => 'Data have been saved successfully.',
+              ];
+            }
+          }
+
+        } catch (\Exception $e){
+          return new JsonResponse( $responseStatus );
+        }
+      }
+    }
+
+    return new JsonResponse( $responseStatus );
+  }
+
+  /**
+   * Check username if it's already on-process
+   */
+  private function checkUsernameIfInProcess($username) {
+    $query = $this->connection->select('casino_optin_report', 'opt')
+                          ->fields('opt', array('username'))
+                          ->condition('opt.username', $username);
+
+    $result = $query->execute();
+    $data = $result->fetchField();
+
+    if ($data) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
---
+++ web/sites/casino-gold/modules/custom/casino_optin_provision/src/Controller/DownloadResultsController.php
@@ -0,0 +1,104 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\casino_optin_provision\Controller\DownloadResultsController.
+ */
+
+namespace Drupal\casino_optin_provision\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class DownloadResultsController extends ControllerBase {
+
+  /**
+   * $connection
+   */
+  protected $connection;
+
+  /**
+   * $request
+   */
+  protected $request;
+
+  /**
+   * $dateFormatter
+   */
+  protected $dateFormatter;
+
+
+  /**
+   *
+   */
+  public function __construct($conn, $date_formatter, RequestStack $req){
+    $this->connection = $conn;
+    $this->dateFormatter = $date_formatter;
+    $this->request = $req;
+  }
+
+  /**
+   *
+   */
+  public static function create( ContainerInterface $container) {
+    return new static(
+      $container->get('database'),
+      $container->get('date.formatter'),
+      $container->get('request_stack')
+    );
+  }
+
+  /**
+   *
+   */
+  public function download() {
+
+    $data = array();
+
+    $dateFrom = $this->request->getCurrentRequest()->get('date_from');
+    $dateTo = $this->request->getCurrentRequest()->get('date_to');
+
+    $tFrom = strtotime("$dateFrom 00:00:01");
+    $tTo = strtotime("$dateTo 23:59:59");
+
+    $query = $this->connection
+                  ->select('casino_optin_report', 'opt')
+                  ->fields('opt', array('oid','username','application_date','currency'))
+                  ->condition('opt.application_date', array($tFrom, $tTo), 'BETWEEN')
+                  ->orderBy('oid', 'DESC');
+
+    $queryResults = $query->execute()->fetchAll();
+
+    foreach ($queryResults as $key => $value) {
+      $data[$key]['application_date'] = $this->dateFormatter->format($value->application_date, 'date_only');
+      $data[$key]['username'] = $value->username;
+      $data[$key]['currrency'] = $value->currency;
+    }
+
+    $this->generateCsv($data, "OptInreport-$dateFrom-$dateTo.csv");
+  }
+
+  /**
+   * Generate CSV File
+   */
+  public function generateCsv($data, $filename = "export.csv") {
+
+    header("Content-Type: text/csv");
+    header("Content-Disposition: attachment; filename=$filename");
+    header("Cache-Control: no-cache, no-store, must-revalidate");
+    header("Pragma: no-cache");
+    header("Expires: 0");
+
+    $output = fopen("php://output", "w");
+
+    fputcsv($output, array('Opt-In Date','Username','Currency'));
+    foreach ($data as $row) {
+        fputcsv($output, $row);
+    }
+
+    fclose($output);
+    exit;
+  }
+
+}
