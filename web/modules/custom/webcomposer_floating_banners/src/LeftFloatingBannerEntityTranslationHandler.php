c14de2067c0985407967cedf89dd091378fa9f45
Aries De Los Santos <aries.delossantos@bayviewtechnology.com>
2017-06-15 14:24:51 +0800

    [CGOII]: Merged working to sprint

---
+++ web/modules/custom/webcomposer_floating_banners/src/LeftFloatingBannerEntityTranslationHandler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\webcomposer_floating_banners;
+
+use Drupal\content_translation\ContentTranslationHandler;
+
+/**
+ * Defines the translation handler for left_floating_banner_entity.
+ */
+class LeftFloatingBannerEntityTranslationHandler extends ContentTranslationHandler {
+
+  // Override here the needed methods from ContentTranslationHandler.
+
+}
