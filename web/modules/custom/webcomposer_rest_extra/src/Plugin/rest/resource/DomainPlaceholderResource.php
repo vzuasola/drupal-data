be3264d10458903a4c48d65ef504d2e94b633d84
Aries De Los Santos <aries.delossantos@bayviewtechnology.com>
2017-06-15 11:51:04 +0800

    [CGOII-972]: code review

--- web/modules/custom/webcomposer_rest_extra/src/Plugin/rest/resource/DomainPlaceholderResource.php
+++ web/modules/custom/webcomposer_rest_extra/src/Plugin/rest/resource/DomainPlaceholderResource.php
@@ -123,8 +123,9 @@ class DomainPlaceholderResource extends ResourceBase {
       $query = \Drupal::entityQuery('taxonomy_term');
       $query->condition('vid', "$vid");
       $tids = $query->execute();
+      $terms = \Drupal\taxonomy\Entity\Term::loadMultiple($tids);
       
-      if ($terms = \Drupal\taxonomy\Entity\Term::loadMultiple($tids)) {
+      if ($terms) {
         $term = reset($terms);
         $getEntities = $term->get("$field")->referencedEntities();
         
