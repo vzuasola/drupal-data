ec69cf56ef2d803dcbd7cea359d2eb1003332615
Alexander Nikko Tenepere <alexandernikko.tenepere@bayviewtechnology.com>
2017-06-19 14:26:05 +0800

    Fixed node serializer taxonomy translation

--- web/modules/custom/webcomposer_rest_extra/src/Plugin/views/style/NodeListSerializer.php
+++ web/modules/custom/webcomposer_rest_extra/src/Plugin/views/style/NodeListSerializer.php
@@ -70,12 +70,22 @@ class NodeListSerializer extends Serializer {
    * Load terms by taxonomy ID
    */
   private function loadTerm($tid) {
+    $lang = \Drupal::languageManager()->getCurrentLanguage(\Drupal\Core\Language\LanguageInterface::TYPE_CONTENT)->getId();
+
     $term = \Drupal\taxonomy\Entity\Term::load($tid);
-
+    $term_translated = \Drupal::service('entity.repository')->getTranslationFromContext($term, $lang); 
+     
     $term_alias = \Drupal::service('path.alias_manager')->getAliasByPath('/taxonomy/term/' . $tid);
-    $term->set('path', $term_alias);
+    $term_translated->set('path', $term_alias);
 
-    return $term;
+    return $term_translated;
+
+    // $term = \Drupal\taxonomy\Entity\Term::load($tid);
+
+    // $term_alias = \Drupal::service('path.alias_manager')->getAliasByPath('/taxonomy/term/' . $tid);
+    // $term->set('path', $term_alias);
+
+    // return $term;
   }
 
    /**
