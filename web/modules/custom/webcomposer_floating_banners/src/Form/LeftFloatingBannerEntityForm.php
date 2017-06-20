49df73ad44dc39082addf3f74cbe385ca20f9e4e
Alexander Nikko Tenepere <alexandernikko.tenepere@bayviewtechnology.com>
2017-06-20 11:00:05 +0800

    Changed settings location of floating banners

--- web/modules/custom/webcomposer_floating_banners/src/Form/LeftFloatingBannerEntityForm.php
+++ web/modules/custom/webcomposer_floating_banners/src/Form/LeftFloatingBannerEntityForm.php
@@ -34,13 +34,13 @@ class LeftFloatingBannerEntityForm extends ContentEntityForm {
 
     switch ($status) {
       case SAVED_NEW:
-        drupal_set_message($this->t('Created the %label Left floating banner entity.', [
+        drupal_set_message($this->t('Created the %label Floating Banner entity.', [
           '%label' => $entity->label(),
         ]));
         break;
 
       default:
-        drupal_set_message($this->t('Saved the %label Left floating banner entity.', [
+        drupal_set_message($this->t('Saved the %label Floating Banner entity.', [
           '%label' => $entity->label(),
         ]));
     }
