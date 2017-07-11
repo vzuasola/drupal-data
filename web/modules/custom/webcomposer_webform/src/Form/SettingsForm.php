630fade25bca1feb315df0402abc311dab8d572c
Alexander Nikko Tenepere <alexandernikko.tenepere@bayviewtechnology.com>
2017-07-11 17:07:22 +0800

    Fix wrong dependency for webcomposer webform settings

--- web/modules/custom/webcomposer_webform/src/Form/SettingsForm.php
+++ web/modules/custom/webcomposer_webform/src/Form/SettingsForm.php
@@ -50,44 +50,48 @@ class SettingsForm {
     $form['third_party_settings']['#weight'] = -5;
 
     // Layout settings
-    $form['third_party_settings']['webcomposer_webform'] = [
+
+    $configs = $settings->getThirdPartySetting('webcomposer_webform', 'webcomposer_webform_layout');
+
+    $form['third_party_settings']['webcomposer_webform']['webcomposer_webform_layout'] = [
       '#type' => 'details',
       '#title' => t('Layout Settings'),
       '#collapsed' => FALSE,
     ];
 
-    $form['third_party_settings']['webcomposer_webform']['label_font_color'] = [
+    $form['third_party_settings']['webcomposer_webform']['webcomposer_webform_layout']['label_font_color'] = [
       '#type' => 'color',
       '#title' => t('Label Font Color'),
       '#description' => t('The color of the field labels'),
-      '#default_value' => $settings->getThirdPartySetting('webcomposer_webform', 'label_font_color', '#000000'),
+      '#default_value' => $configs['label_font_color'] ?? '#000000',
     ];
 
-    $form['third_party_settings']['webcomposer_webform']['error_font_color'] = [
+    $form['third_party_settings']['webcomposer_webform']['webcomposer_webform_layout']['error_font_color'] = [
       '#type' => 'color',
       '#title' => t('Error Font Color'),
       '#description' => t('The color of the error labels'),
-      '#default_value' => $settings->getThirdPartySetting('webcomposer_webform', 'error_font_color', '#ff0000'),
+      '#default_value' => $configs['error_font_color'] ?? '#ff0000',
     ];
 
-    $d = $settings->getThirdPartySetting('webcomposer_webform', 'header_markup');
-
-    $form['third_party_settings']['webcomposer_webform']['header_markup'] = [
+    $form['third_party_settings']['webcomposer_webform']['webcomposer_webform_layout']['header_markup'] = [
       '#type' => 'text_format',
       '#title' => t('Header Markup'),
       '#description' => t('An optional markup to show as the form header'),
-      '#default_value' => $d['value'] ?? NULL,
-      '#format' => $d['format'] ?? NULL,
+      '#default_value' => $configs['header_markup']['value'] ?? NULL,
+      '#format' => $configs['header_markup']['format'] ?? NULL,
     ];
 
     // Submission settings
-    $form['third_party_settings']['webcomposer_webform_submission'] = [
+
+    $configs = $settings->getThirdPartySetting('webcomposer_webform', 'webcomposer_webform_submission');
+
+    $form['third_party_settings']['webcomposer_webform']['webcomposer_webform_submission'] = [
       '#type' => 'details',
       '#title' => t('Submission Settings'),
       '#collapsed' => FALSE,
     ];
 
-    $form['third_party_settings']['webcomposer_webform_submission']['submission_status'] = [
+    $form['third_party_settings']['webcomposer_webform']['webcomposer_webform_submission']['submission_status'] = [
       '#type' => 'radios',
       '#title' => t('Submission Status'),
       '#description' => t('The status for this form submission'),
@@ -95,82 +99,89 @@ class SettingsForm {
         'open' => 'Open',
         'close' => 'Closed',
       ],
-      '#default_value' => $settings->getThirdPartySetting('webcomposer_webform_submission', 'submission_status', 'open'),
+      '#default_value' => $configs['submission_status'] ?? 'open',
     ];
 
-    $form['third_party_settings']['webcomposer_webform_submission']['successful_submission_message'] = [
+    $form['third_party_settings']['webcomposer_webform']['webcomposer_webform_submission']['successful_submission_message'] = [
       '#type' => 'textarea',
       '#title' => t('Successful Submission Message'),
       '#description' => t('Message to show when the submission is successful'),
-      '#default_value' => $settings->getThirdPartySetting('webcomposer_webform_submission', 'successful_submission_message'),
+      '#default_value' => $configs['successful_submission_message'] ?? NULL,
     ];
 
-    $form['third_party_settings']['webcomposer_webform_submission']['closed_submission_message'] = [
+    $form['third_party_settings']['webcomposer_webform']['webcomposer_webform_submission']['closed_submission_message'] = [
       '#type' => 'textarea',
       '#title' => t('Closed Submission Message'),
       '#description' => t('Message to show when the submission is closed'),
-      '#default_value' => $settings->getThirdPartySetting('webcomposer_webform_submission', 'closed_submission_message'),
+      '#default_value' => $configs['closed_submission_message'] ?? NULL,
     ];
 
-    $form['third_party_settings']['webcomposer_webform_submission']['failed_submission_message'] = [
+    $form['third_party_settings']['webcomposer_webform']['webcomposer_webform_submission']['failed_submission_message'] = [
       '#type' => 'textarea',
       '#title' => t('Failed Submission Message'),
       '#description' => t('Message to show when the submission is failed'),
-      '#default_value' => $settings->getThirdPartySetting('webcomposer_webform_submission', 'failed_submission_message'),
+      '#default_value' => $configs['failed_submission_message'] ?? NULL,
     ];
 
     // Submission Layout settings
-    $form['third_party_settings']['webcomposer_webform_submission_layout'] = [
+
+    $configs = $settings->getThirdPartySetting('webcomposer_webform', 'webcomposer_webform_submission_layout');
+
+    $form['third_party_settings']['webcomposer_webform']['webcomposer_webform_submission_layout'] = [
       '#type' => 'details',
       '#title' => t('Submission Layout Settings'),
       '#collapsed' => FALSE,
     ];
 
-    $form['third_party_settings']['webcomposer_webform_submission_layout']['success_font_color'] = [
+    $form['third_party_settings']['webcomposer_webform']['webcomposer_webform_submission_layout']['success_font_color'] = [
       '#type' => 'color',
       '#title' => t('Success Font Color'),
       '#description' => t('The color of the text for success message'),
-      '#default_value' => $settings->getThirdPartySetting('webcomposer_webform_submission_layout', 'success_font_color', '#ffffff'),
+      '#default_value' => $configs['success_font_color'] ?? '#ffffff',
     ];
 
-    $form['third_party_settings']['webcomposer_webform_submission_layout']['success_background_color'] = [
+    $form['third_party_settings']['webcomposer_webform']['webcomposer_webform_submission_layout']['success_background_color'] = [
       '#type' => 'color',
       '#title' => t('Success Background Color'),
       '#description' => t('The background color of the success message'),
-      '#default_value' => $settings->getThirdPartySetting('webcomposer_webform_submission_layout', 'success_background_color', '#008000'),
+      '#default_value' => $configs['success_background_color'] ?? '#008000',
     ];
 
-    $form['third_party_settings']['webcomposer_webform_submission_layout']['error_font_color'] = [
+    $form['third_party_settings']['webcomposer_webform']['webcomposer_webform_submission_layout']['error_font_color'] = [
       '#type' => 'color',
       '#title' => t('Error Font Color'),
       '#description' => t('The color of the error labels'),
-      '#default_value' => $settings->getThirdPartySetting('webcomposer_webform_submission_layout', 'error_font_color', '#ffffff'),
+      '#default_value' => $configs['error_font_color'] ?? '#ffffff',
     ];
 
-    $form['third_party_settings']['webcomposer_webform_submission_layout']['error_background_color'] = [
+    $form['third_party_settings']['webcomposer_webform']['webcomposer_webform_submission_layout']['error_background_color'] = [
       '#type' => 'color',
       '#title' => t('Error Background Color'),
       '#description' => t('The background color of the error message'),
-      '#default_value' => $settings->getThirdPartySetting('webcomposer_webform_submission_layout', 'error_background_color', '#ff0000'),
+      '#default_value' => $configs['error_background_color'] ?? '#ff0000',
     ];
 
-    $form['third_party_settings']['webform_background'] = [
+    // Webform backgrounds
+
+    $configs = $settings->getThirdPartySetting('webcomposer_webform', 'webform_background');
+
+    $form['third_party_settings']['webcomposer_webform']['webform_background'] = [
       '#type' => 'details',
       '#title' => t('Background Settings'),
       '#open' => FALSE,
     ];
 
-    $form['third_party_settings']['webform_background']['background_image'] = [
+    $form['third_party_settings']['webcomposer_webform']['webform_background']['background_image'] = [
       '#type' => 'webform_image_file',
       '#title' => t('Default Form Background Image'),
       '#description' => t('The background image of the form'),
-      '#default_value' => $settings->getThirdPartySetting('webform_background', 'background_image'),
+      '#default_value' => $configs['background_image'] ?? NULL,
       '#upload_location' => 'public://webform-backgrounds',
     ];
 
-    // translated backgrounds
+    // Translated backgrounds
 
-    $form['third_party_settings']['webform_background']['translated'] = [
+    $form['third_party_settings']['webcomposer_webform']['webform_background']['translated'] = [
       '#type' => 'details',
       '#title' => t('Translated Backgrounds'),
       '#open' => FALSE,
@@ -183,11 +194,11 @@ class SettingsForm {
       if (isset($this->prefixes[$lang])) {
         $langKey = $this->prefixes[$lang];
 
-        $form['third_party_settings']['webform_background']['translated']["background_image_$langKey"] = [
+        $form['third_party_settings']['webcomposer_webform']['webform_background']['translated']["background_image_$langKey"] = [
           '#type' => 'webform_image_file',
           '#title' => "Form Background Image for " . strtoupper($langKey) ,
           '#description' => t('The background image of the form'),
-          '#default_value' => $settings->getThirdPartySetting('webform_background', "background_image_$langKey"),
+          '#default_value' => $configs["background_image_$langKey"] ?? NULL,
           '#upload_location' => 'public://webform-backgrounds',
         ];
       }
@@ -227,8 +238,8 @@ class SettingsForm {
       if (isset($this->prefixes[$lang])) {
         $langKey = $this->prefixes[$lang];
 
-        if (empty($third_party_settings['webform_background']["background_image_$langKey"])) {
-          unset($third_party_settings['webform_background']["background_image_$langKey"]);
+        if (empty($third_party_settings['webform_background']['webform_background']["background_image_$langKey"])) {
+          unset($third_party_settings['webform_background']['webform_background']["background_image_$langKey"]);
         }
       }
     }
