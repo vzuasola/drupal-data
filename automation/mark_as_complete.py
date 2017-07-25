fe0d68049421c5850f6050867c0bd573a98b313b
Eric Van Steenbergen <eric.vansteenbergen@esl-asia.com>
2017-07-25 09:02:37 +0800

    Initial commit

---
+++ automation/mark_as_complete.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+"""
+marks a step as complete
+"""
+
+import os
+import sys
+import json
+import datetime
+from lib.logger import logger
+
+
+def main():
+    filename = os.environ['CREATES']
+    data = {'date': str(datetime.date.today())}
+    with open(filename, 'a') as data_out:
+        data_out.write(json.dumps(data))
+    logger.debug('add %s to %s', data, filename)
+    logger.info('stage complete')
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as error:
+        logger.error('failed to mark this stage as complete')
+        sys.exit(1)
