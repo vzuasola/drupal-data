cf71c124bd502f409f2eb2987c16cdb45b453fef
kbhagwat <kaustubh.bhagwat@bayviewtechnology.com>
2017-08-10 13:12:16 +0800

    WBC-548 Created content entity for WBC slider functionality

---
+++ web/modules/custom/webcomposer_slider/src/Entity/WebComposerSliderTypeInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\webcomposer_slider\Entity;
+
+use Drupal\Core\Config\Entity\ConfigEntityInterface;
+
+/**
+ * Provides an interface for defining Web Composer Slider type entities.
+ */
+interface WebComposerSliderTypeInterface extends ConfigEntityInterface {
+
+  // Add get/set methods for your configuration properties here.
+}
---
+++ web/modules/custom/webcomposer_slider/src/WebComposerSliderTranslationHandler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\webcomposer_slider;
+
+use Drupal\content_translation\ContentTranslationHandler;
+
+/**
+ * Defines the translation handler for web_composer_slider.
+ */
+class WebComposerSliderTranslationHandler extends ContentTranslationHandler {
+
+  // Override here the needed methods from ContentTranslationHandler.
+
+}
