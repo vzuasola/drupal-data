0e6d45ffb82cb6caca756272c841b4d425005b86
Ashwini Kumar <ashwini.kumar@bayviewtechnology.com>
2017-08-23 15:09:49 +0800

    Added variable to settings php for entrypage.

--- config/entrypage.settings.php
+++ config/entrypage.settings.php
@@ -760,3 +760,4 @@ $config_directories['sync'] = 'sites/entrypage/config/sync';
 
 // The primary site of front end
 $settings['primary_site_prefix'] = 'promotions';
+$settings['cke_editor_config'] = '/en/promotions';
