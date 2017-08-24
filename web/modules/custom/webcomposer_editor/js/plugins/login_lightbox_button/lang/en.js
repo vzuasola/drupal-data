e75782d2811d9fbcfbbe65722a765f16ede42c13
Aries De Los Santos <aries.delossantos@bayviewtechnology.com>
2017-08-24 14:31:40 +0800

    [CCL2-30]: added widget to generate login button

---
+++ web/modules/custom/webcomposer_editor/js/plugins/login_lightbox_button/lang/en.js
@@ -0,0 +1,18 @@
+CKEDITOR.plugins.setLang( 'login_lightbox_button', 'en', {
+    loginLightboxButton: {
+        label: 'Login Light Box Button'
+    },
+    loginLightboxButtonFields: {
+        label: {
+            label: 'Label',
+            notEmptyError: 'Label cannot be empty.'
+        },
+        // alias: {
+        //     label: 'Alias',
+        //     notEmptyError: 'Alias cannot be empty.'
+        // },
+        class: {
+            label: 'Class'
+        }
+    }
+});
