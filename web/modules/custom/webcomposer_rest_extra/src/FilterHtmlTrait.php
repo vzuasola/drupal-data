ca832ed9a6ede257d6e4bdd5d98bd9b3b1d6a75a
Aries De Los Santos <aries.delossantos@bayviewtechnology.com>
2017-09-28 12:30:58 +0800

    [CCL2]: added trait for filtering image

---
+++ web/modules/custom/webcomposer_rest_extra/src/FilterHtmlTrait.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\webcomposer_rest_extra;
+
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Site\Settings;
+
+/**
+ * Provide a trait to filter html
+ */
+trait FilterHtmlTrait {
+
+    /**
+    * Filtered Html for Image Source.
+    */
+    public function filterHtml($markup) {
+        $document = new Html();
+
+        $htmlDoc = $document->load($markup);
+        $domObject = simplexml_import_dom($htmlDoc);
+
+        $images = $domObject->xpath('//img');
+        $basePath = Settings::get('ck_editor_inline_image_prefix', NULL);
+
+        foreach ($images as $image) {
+          $replace = preg_replace('/\/sites\/[a-z\-]+\/files/', $basePath, $image['src']);
+          $image['src'] = $replace;
+        }
+
+        $htmlMarkup = Html::serialize($htmlDoc);
+        $processedHtml = trim($htmlMarkup);
+
+        return $processedHtml;
+    }
+}
