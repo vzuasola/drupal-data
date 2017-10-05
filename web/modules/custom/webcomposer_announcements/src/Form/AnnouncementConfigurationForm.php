05a2ac362e1249bc1e54daafff0b31fb319dfdb3
Ashwini Kumar <ashwini.kumar@bayviewtechnology.com>
2017-10-05 18:56:41 +0800

    Fixed phpcs error and warning for wbc announcement module.

--- web/modules/custom/webcomposer_announcements/src/Form/AnnouncementConfigurationForm.php
+++ web/modules/custom/webcomposer_announcements/src/Form/AnnouncementConfigurationForm.php
@@ -4,9 +4,12 @@ namespace Drupal\webcomposer_announcements\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\file\Entity\File;
 
+/**
+ * Config form for Announcement feature.
+ */
 class AnnouncementConfigurationForm extends ConfigFormBase {
+
   /**
    * {@inheritdoc}
    */
@@ -27,21 +30,21 @@ class AnnouncementConfigurationForm extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('webcomposer_config.announcements_configuration');
 
-    $form['title'] = array(
+    $form['title'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Title'),
       '#description' => $this->t('Title of the announcement lightbox.'),
       '#default_value' => $config->get('title'),
       '#required' => TRUE,
-    );
+    ];
 
-    $form['default_message'] = array(
+    $form['default_message'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Default Message'),
       '#description' => $this->t('Message to display inside the lightbox when there is no published announcement.'),
       '#default_value' => $config->get('default_message'),
       '#required' => TRUE,
-    );
+    ];
 
     return parent::buildForm($form, $form_state);
   }
@@ -51,11 +54,11 @@ class AnnouncementConfigurationForm extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     parent::submitForm($form, $form_state);
-    
-    $keys = array(
+
+    $keys = [
       'title',
       'default_message',
-    );
+    ];
 
     foreach ($keys as $key) {
       $this->config('webcomposer_config.announcements_configuration')->set($key, $form_state->getValue($key))->save();
