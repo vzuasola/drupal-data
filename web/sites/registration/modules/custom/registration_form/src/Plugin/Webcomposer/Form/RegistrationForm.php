ef5b7ce20702f72bd9392d36bfc32a88d43fb07a
Rainer Cedric Mariano <RainerCedric.Mariano@bayviewtechnology.com>
2017-10-12 13:32:42 +0800

    [CREG2-129] Commit form definition settings

--- web/sites/registration/modules/custom/registration_form/src/Plugin/Webcomposer/Form/RegistrationForm.php
+++ web/sites/registration/modules/custom/registration_form/src/Plugin/Webcomposer/Form/RegistrationForm.php
@@ -10,69 +10,142 @@ use Drupal\webcomposer_form_manager\WebcomposerFormInterface;
  *
  * @WebcomposerForm(
  *   id = "registration",
- *   name = "RegistrationForm",
+ *   name = "Registration Form",
  * )
  */
-class Registration extends WebcomposerFormBase implements WebcomposerFormInterface {
-  /**
-   *
-   */
-  public function getSettings() {
-    return [
-      'show' => [
-        '#title' => 'Show this form',
-        '#type' => 'checkbox',
-        '#default_value' => true
-      ],
-      'alias' => [
-        '#title' => 'Registration Form',
-        '#type' => 'textfield',
-        '#description' => 'Registration Alias',
-      ],
-    ];
-  }
+class RegistrationForm extends WebcomposerFormBase implements WebcomposerFormInterface {
 
   /**
    *
    */
-  public function getFields() {
-    return [
-      'username' => [
-        'name' => 'Username',
-        'type' => 'textfield',
-        'options' => [
-          'default_value' => 'Leandrew',
-        ],
-        'settings' => [
-          'alias' => [
-            '#title' => 'Username alias',
-            '#type' => 'textfield',
-            '#description' => 'The alias for this username',
-          ],
-        ],
-      ],
+    public function getSettings() {
+        return [
+                'show' => [
+                '#title' => 'Show this form',
+                '#type' => 'checkbox',
+                '#default_value' => true
+            ],
+                'alias' => [
+                '#title' => 'Registration Form',
+                '#type' => 'textfield',
+                '#description' => 'Registration Alias',
+            ],
+        ];
+    }
 
-      'password' => [
-        'name' => 'Password',
-        'type' => 'textfield',
-      ],
+    /**
+    *
+    */
+    public function getFields() {
+        return [
+            'username' => [
+                'name' => 'Username',
+                'type' => 'textfield',
+                'options' => [
+                  'default_value' => 'Leandrew',
+                ],
+                'settings' => [
+                    'alias' => [
+                        '#title' => 'Username alias',
+                        '#type' => 'textfield',
+                        '#description' => 'Default name for Username field',
+                    ],
+                ],
+            ],
 
-      'email' => [
-        'name' => 'Email',
-        'type' => 'textfield',
-      ],
+            'password' => [
+                'name' => 'Password',
+                'type' => 'textfield',
+                'settings' => [
+                    'alias' => [
+                        '#title' => 'Password alias',
+                        '#type' => 'textfield',
+                        '#description' => 'Default name for Password field',
+                    ],
+                ],
+            ],
 
-      'comments' => [
-        'name' => 'Comment',
-        'type' => 'textfield',
-        'settings' => [
-          'name' => [
-            '#title' => 'Default name',
-            '#type' => 'textfield',
-            '#description' => 'The default name for the comment',
-          ],
-        ],
-      ],
-    ];
-  }
+            'email' => [
+                'name' => 'Email',
+                'type' => 'textfield',
+                'settings' => [
+                    'alias' => [
+                        '#title' => 'Email alias',
+                        '#type' => 'textfield',
+                        '#description' => 'Default Name for Email field',
+                    ],
+                ],
+            ],
+
+            'mobile_number' => [
+                'name' => 'Mobile Number',
+                'type' => 'textfield',
+                'settings' => [
+                    'name' => [
+                        '#title' => 'Mobile Number',
+                        '#type' => 'textfield',
+                        '#description' => 'The default name for the Mobile Number field',
+                    ],
+                ],
+            ],
+
+            'first_name' => [
+                'name' => 'First Name',
+                'type' => 'textfield',
+                'settings' => [
+                    'name' => [
+                        '#title' => 'First Name',
+                        '#type' => 'textfield',
+                        '#description' => 'The default name for the First Name field',
+                    ],
+                ],
+            ],
+
+            'last_name' => [
+                'name' => 'Last Name',
+                'type' => 'textfield',
+                'settings' => [
+                    'name' => [
+                        '#title' => 'Last Name',
+                        '#type' => 'textfield',
+                        '#description' => 'The default name for the Last Name field',
+                    ],
+                ],
+            ],
+
+            'currency' => [
+                'name' => 'Currency',
+                'type' => 'select',
+                'settings' => [
+                    'name' => [
+                        '#title' => 'Currency',
+                        '#type' => 'textfield',
+                        '#description' => 'The default name for the Currency field',
+                    ],
+                    'default_name' => [
+                        '#title' => 'Default Name',
+                        '#type' => 'textfield',
+                        '#description' => 'The Empty value name for the Currency dropdown field',
+                    ],
+                ],
+            ],
+
+           'country' => [
+                'name' => 'Country',
+                'type' => 'select',
+                'settings' => [
+                    'name' => [
+                        '#title' => 'Country',
+                        '#type' => 'textfield',
+                        '#description' => 'The default name for the Country field',
+                    ],
+                    'default_name' => [
+                        '#title' => 'Default Name',
+                        '#type' => 'textfield',
+                        '#description' => 'The Empty value name for the Country dropdown field',
+                    ],
+                ],
+            ],
+        ];
+    }
 }
