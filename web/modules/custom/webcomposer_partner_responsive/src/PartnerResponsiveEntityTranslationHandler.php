7775b8c436ef349608c71901085fa578eac6426c
Rainer Cedric Mariano <RainerCedric.Mariano@bayviewtechnology.com>
2017-10-13 18:53:40 +0800

    [CREG2-134] Initial commit for webcomposer partners responsive logo module

---
+++ web/modules/custom/webcomposer_partner_responsive/src/PartnerResponsiveEntityTranslationHandler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\webcomposer_partner_responsive;
+
+use Drupal\content_translation\ContentTranslationHandler;
+
+/**
+ * Defines the translation handler for partner_responsive_entity.
+ */
+class PartnerResponsiveEntityTranslationHandler extends ContentTranslationHandler {
+
+  // Override here the needed methods from ContentTranslationHandler.
+
+}
