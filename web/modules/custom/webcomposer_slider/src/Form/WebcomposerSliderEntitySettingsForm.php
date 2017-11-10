5127eb2811e8f128de2ade723bb05ef0a7f3f90d
Ariel Alvarez <Ariel.Alvarez@bayviewtechnology.com>
2017-11-10 15:01:22 +0800

    Addes slider settings.

--- web/modules/custom/webcomposer_slider/src/Form/WebcomposerSliderEntitySettingsForm.php
+++ web/modules/custom/webcomposer_slider/src/Form/WebcomposerSliderEntitySettingsForm.php
@@ -2,7 +2,7 @@
 
 namespace Drupal\webcomposer_slider\Form;
 
-use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
@@ -12,7 +12,14 @@ use Drupal\Core\Form\FormStateInterface;
  *
  * @ingroup webcomposer_slider
  */
-class WebcomposerSliderEntitySettingsForm extends FormBase {
+class WebcomposerSliderEntitySettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['webcomposer_config.slider_configuration'];
+  }
 
   /**
    * Returns a unique string identifying the form.
@@ -21,19 +28,7 @@ class WebcomposerSliderEntitySettingsForm extends FormBase {
    *   The unique string identifying the form.
    */
   public function getFormId() {
-    return 'WebcomposerSliderEntity_settings';
-  }
-
-  /**
-   * Form submission handler.
-   *
-   * @param array $form
-   *   An associative array containing the structure of the form.
-   * @param \Drupal\Core\Form\FormStateInterface $form_state
-   *   The current state of the form.
-   */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-    // Empty implementation of the abstract submit class.
+    return 'webcomposer_slider';
   }
 
   /**
@@ -48,8 +43,48 @@ class WebcomposerSliderEntitySettingsForm extends FormBase {
    *   Form definition array.
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('webcomposer_config.slider_configuration');
+
     $form['WebcomposerSliderEntity_settings']['#markup'] = 'Settings form for Webcomposer slider entity entities. Manage field settings here.';
-    return $form;
+
+    $form['WebcomposerSliderEntity_settings']['enable_collapsible_slider'] = [
+      '#type' => 'checkbox', 
+      '#title' => $this->t('Enable Collapsible Slider.'),
+      '#default_value' => $config->get('enable_collapsible_slider'),
+    ];
+
+    $form['WebcomposerSliderEntity_settings']['slider_pager_position'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Slider Pager Position'),
+      '#options' => [
+        'left' => 'Left',
+        'center' => 'Center',
+        'right' => 'Right',
+      ],
+      '#default_value' => $config->get('slider_pager_position') ? $config->get('slider_pager_position') : 'center',
+    ];
+
+    return parent::buildForm($form, $form_state);
   }
 
+  /**
+   * Form submission handler.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $keys = [
+      'slider_pager_position',
+      'enable_collapsible_slider',
+    ];
+
+    foreach ($keys as $key) {
+      $this->config('webcomposer_config.slider_configuration')->set($key, $form_state->getValue($key))->save();
+    }
+
+    return parent::submitForm($form, $form_state);
+  }
 }
