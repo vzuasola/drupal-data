7f1d32f7c1805d0361c1638432e396379e387820
Yunyce de Jesus <Yunyce.DeJesus@bayviewtechnology.com>
2017-11-20 09:35:28 +0800

    [feature-recommendation] Added CMS custom config for games search recommendation

---
+++ web/sites/casino/modules/custom/casino_games_search_recommendation/src/Form/GamesRecommendationForm.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Drupal\casino_games_search_recommendation\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\file\Entity\File;
+
+class GamesRecommendationForm extends ConfigFormBase {
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['casino_games_search_recommendation.recommendation_configuration'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'casino_games_search_recommendation.recommendation_configuration_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('casino_games_search_recommendation.recommendation_configuration');
+
+    $form['recommendation_configuration_tab'] = array(
+      '#type' => 'vertical_tabs',
+      '#title' => t('Settings'),
+    );
+
+    $form['recommendations'] = array(
+      '#type' => 'details',
+      '#title' => $this->t('Recommendation Settings'),
+      '#collapsible' => TRUE,
+      '#group' => 'recommendation_configuration_tab'
+    );
+
+    $form['recommendations']['recommendation_category_tid'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Recommendation Category Taxonomy ID'),
+      '#description' => $this->t('Recommendation Category Taxonomy ID'),
+      '#default_value' => $config->get('recommendation_category_tid'),
+      '#required' => TRUE,
+    );
+
+    $recommendation_message = $config->get('recommendation_message');
+    $form['recommendations']['recommendation_message'] = array(
+      '#type' => 'text_format',
+      '#title' => $this->t('Recommendation Message'),
+      '#description' => $this->t('The text to display on recommendation message.'),
+      '#default_value' => $recommendation_message['value'],
+      '#format' => $recommendation_message['format'],
+      '#required' => TRUE,
+    );
+
+    $recommendation_message_negative = $config->get('recommendation_message_negative');
+    $form['recommendations']['recommendation_message_negative'] = array(
+      '#type' => 'text_format',
+      '#title' => $this->t('Recommendation Not Set Message'),
+      '#description' => $this->t('The text to display on recommendation message when no recommendation is set.'),
+      '#default_value' => $recommendation_message_negative['value'],
+      '#format' => $recommendation_message_negative['format'],
+      '#required' => TRUE,
+    );
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    parent::submitForm($form, $form_state);
+
+    $keys = array(
+      'recommendation_category_tid',
+      'recommendation_message',
+      'recommendation_message_negative',
+    );
+
+    foreach ($keys as $key) {
+      $this->config('casino_games_search_recommendation.recommendation_configuration')->set($key, $form_state->getValue($key))->save();
+    }
+  }
+
+}
