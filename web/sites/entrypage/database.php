4d4ad5e51d8c3a9c9198a8b609fcd56d017ff565
Darryl Mabini <darryl.mabini@bayviewtechnology.com>
2017-11-22 09:49:08 +0800

    Sync draggableviews on submit arguments and join arguments

---
+++ web/sites/entrypage/database.php
@@ -0,0 +1,12 @@
+<?php
+
+$databases['default']['default'] = array (
+  'database' => 'entrypage',
+  'username' => 'admin',
+  'password' => 'admin',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
