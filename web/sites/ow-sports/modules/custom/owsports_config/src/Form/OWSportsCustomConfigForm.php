abb746f73c3eec11f2a71eef8d666f979fa779ae
Glenn Fundimera <glenn.fundimera@bayviewtechnology.com>
2018-01-26 14:30:09 +0800

    Remove config euro switch template

--- web/sites/ow-sports/modules/custom/owsports_config/src/Form/OWSportsCustomConfigForm.php
+++ web/sites/ow-sports/modules/custom/owsports_config/src/Form/OWSportsCustomConfigForm.php
@@ -171,13 +171,6 @@ class OWSportsCustomConfigForm extends ConfigFormBase {
       '#default_value' => $config->get('euro_template'),
     ];
 
-    $form['euro_config_group']['euro_switch_redirect'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Override Switch Template'),
-      '#description' => $this->t('Override switch template via page redirection. Default switch template via iframe.'),
-      '#default_value' => $config->get('euro_switch_redirect'),
-    ];
-
     $form['singbet_config_group'] = [
       '#type' => 'details',
       '#title' => $this->t('Singbet Template'),
