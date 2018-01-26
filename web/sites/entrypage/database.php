7a36e7009c12cc4af1f7bf18f4fb6c4078f61f2b
Darryl Mabini <darryl.mabini@bayviewtechnology.com>
2018-01-26 18:53:26 +0800

    Update marketing script config

---
+++ web/sites/entrypage/database.php
@@ -0,0 +1,12 @@
+<?php
+
+$databases['default']['default'] = array (
+  'database' => 'entrypage_qa1',
+  'username' => 'root',
+  'password' => 'secret',
+  'prefix' => '',
+  'host' => 'mariadb',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
---
+++ web/sites/ow-sports/database.php
@@ -0,0 +1,12 @@
+<?php
+
+$databases['default']['default'] = array (
+  'database' => 'ow_sports',
+  'username' => 'root',
+  'password' => 'secret',
+  'prefix' => '',
+  'host' => 'mariadb',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
