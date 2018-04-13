fc428fe39e4687814fc08f1628895ec61f6aaf45
jislam <jahirul.islam@bayviewtechnology.com>
2018-04-13 12:22:16 +0800

    Added WBC new form implementation

--- web/sites/live-dealer/modules/custom/games_page_loading/src/Form/GamesPageLoadingForm.php
+++ web/sites/live-dealer/modules/custom/games_page_loading/src/Form/GamesPageLoadingForm.php
@@ -2,11 +2,10 @@
 
 namespace Drupal\games_page_loading\Form;
 
-use Drupal\Core\Form\ConfigFormBase;
+use Drupal\webcomposer_config_schema\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\file\Entity\File;
 
-class GamesPageLoadingForm extends ConfigFormBase {
+class GamesPageLoadingForm extends FormBase {
   /**
    * {@inheritdoc}
    */
@@ -24,58 +23,59 @@ class GamesPageLoadingForm extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state) {
-    $config = $this->config('games_config.games_loading_configuration');
+  public function form(array $form, FormStateInterface $form_state) {
 
-    $form['games_loading_configuration_tab'] = array(
+    $form['games_loading_configuration_tab'] = [
       '#type' => 'vertical_tabs',
       '#title' => t('Settings'),
-    );
+    ];
 
-    $form['loading_message'] = array(
+    $form['loading_message'] = [
       '#type' => 'details',
       '#title' => $this->t('Loading Message Settings'),
       '#collapsible' => TRUE,
       '#group' => 'games_loading_configuration_tab'
-    );
+    ];
 
-    $loading_message_content = $config->get('loading_message_content');
-    $form['loading_message']['loading_message_content'] = array(
+    $loading_message_content = $this->get('loading_message_content');
+    $form['loading_message']['loading_message_content'] = [
       '#type' => 'text_format',
       '#title' => $this->t('Loading Message Content'),
       '#description' => $this->t('The text to display on game page loading message.'),
-      '#default_value' => $loading_message_content['value'],
+      '#default_value' =>  $loading_message_content['value'],
       '#format' => $loading_message_content['format'],
       '#required' => TRUE,
-    );
+      '#translatable' => TRUE,
+    ];
 
-    $loading_error_message = $config->get('loading_error_message');
-    $form['loading_message']['loading_error_message'] = array(
+    $loading_error_message = $this->get('loading_error_message');
+    $form['loading_message']['loading_error_message'] = [
       '#type' => 'text_format',
       '#title' => $this->t('Loading Error Message Content'),
       '#description' => $this->t('The text to display on game page loading upon error.'),
       '#default_value' => $loading_error_message['value'],
       '#format' => $loading_error_message['format'],
       '#required' => TRUE,
-    );
+      '#translatable' => TRUE,
+    ];
 
-    return parent::buildForm($form, $form_state);
+    return $form;
   }
 
   /**
    * {@inheritdoc}
    */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-    parent::submitForm($form, $form_state);
-
-    $keys = array(
+  public function submit(array &$form, FormStateInterface $form_state) {
+    $keys = [
       'loading_message_content',
       'loading_error_message',
-    );
+    ];
+  foreach ($keys as $key) {
+    $data[$key] = $form_state->getValue($key);
+  }
 
-    foreach ($keys as $key) {
-      $this->config('games_config.games_loading_configuration')->set($key, $form_state->getValue($key))->save();
-    }
+  $this->save($data);
+
   }
 
 }
