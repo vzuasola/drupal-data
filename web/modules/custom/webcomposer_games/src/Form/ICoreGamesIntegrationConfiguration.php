07b2bc3e3811778ced2eea382a49ecff5e0c7508
Aries De Los Santos <aries.delossantos@bayviewtechnology.com>
2018-04-17 09:02:13 +0800

    added config for allbet, tgp, evo and ebet ld providers

--- web/modules/custom/webcomposer_games/src/Form/ICoreGamesIntegrationConfiguration.php
+++ web/modules/custom/webcomposer_games/src/Form/ICoreGamesIntegrationConfiguration.php
@@ -25,7 +25,6 @@ class ICoreGamesIntegrationConfiguration extends ConfigFormBase {
         'tgp' => 'TGP',
         'evo_gaming' => 'Evolution Gaming',
         'ebet' => 'eBet',
-
     ];
 
   /**
