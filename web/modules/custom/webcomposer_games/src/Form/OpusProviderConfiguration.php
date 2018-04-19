3e489ae2dbb250d51382fa13497c04be9ef8a26c
skumar <sanjeev.kumar@bayviewtechnology.com>
2018-04-19 12:57:42 +0800

    config for GPI

--- web/modules/custom/webcomposer_games/src/Form/OpusProviderConfiguration.php
+++ web/modules/custom/webcomposer_games/src/Form/OpusProviderConfiguration.php
@@ -31,42 +31,52 @@ class OpusProviderConfiguration extends ConfigFormBase {
 
     $content = $config->get('opus_game_loader_content');
     $message = $config->get('opus_unsupported_currencies_message');
-    $form['opus_fallback_error'] = array(
+    $form['advanced'] = [
+      '#type' => 'vertical_tabs',
+      '#title' => t('Opus and Gpi Configurations'),
+    ];
+
+    $form['opus_gen_config'] = [
+      '#type' => 'details',
+      '#title' => t('Opus General Configurations'),
+      '#group' => 'advanced',
+    ];
+    $form['opus_gen_config']['opus_fallback_error'] = array(
       '#type' => 'textfield',
       '#title' => t('Fallback error'),
       '#description' => $this->t('Fallback Error'),
       '#default_value' => $config->get('opus_fallback_error')
     );
 
-    $form['opus_game_loader_content'] = [
+    $form['opus_gen_config']['opus_game_loader_content'] = [
       '#type' => 'text_format',
       '#title' => $this->t('Game Loader Content'),
       '#default_value' => $content['value'],
       '#format' => $content['format'],
     ];
 
-    $form['opus_game_url'] = array(
+    $form['opus_gen_config']['opus_game_url'] = array(
       '#type' => 'textfield',
       '#title' => t('Opus Game url'),
       '#description' => $this->t('Defines the Game Url'),
       '#default_value' => $config->get('opus_game_url')
     );
 
-    $form['opus_game_free_play_url'] = array(
+    $form['opus_gen_config']['opus_game_free_play_url'] = array(
       '#type' => 'textfield',
       '#title' => t('Opus Free Play Game url'),
       '#description' => $this->t('Defines the Opus Free Play Games Url'),
       '#default_value' => $config->get('opus_game_free_play_url')
     );
 
-    $form['opus_alternative_game_url'] = array(
+    $form['opus_gen_config']['opus_alternative_game_url'] = array(
       '#type' => 'textfield',
       '#title' => t('Opus Alternate Game url'),
       '#description' => $this->t('Defines the alternate game url'),
       '#default_value' => $config->get('opus_alternative_game_url')
     );
 
-    $form['languages'] = array(
+    $form['opus_gen_config']['languages'] = array(
       '#type' => 'textarea',
       '#title' => t('Language Mapping'),
       '#size' => 500,
@@ -78,7 +88,7 @@ class OpusProviderConfiguration extends ConfigFormBase {
       '#default_value' => $config->get('languages')
     );
 
-   $form['currency'] = array(
+   $form['opus_gen_config']['currency'] = array(
       '#type' => 'textarea',
       '#title' => t('Currency'),
       '#size' => 500,
@@ -87,24 +97,47 @@ class OpusProviderConfiguration extends ConfigFormBase {
       '#default_value' => $config->get('currency')
     );
 
-    $form['opus_unsupported_currencies_title'] = array(
+    $form['opus_gen_config']['opus_unsupported_currencies_title'] = array(
       '#type' => 'textfield',
       '#title' => t('Unsupported Currency title'),
       '#description' => $this->t('Defines the Unsupported Currency LightBox title'),
       '#default_value' => $config->get('opus_unsupported_currencies_title')
     );
 
-    $form['opus_unsupported_currencies_message'] = array(
+    $form['opus_gen_config']['opus_unsupported_currencies_message'] = array(
       '#type' => 'text_format',
       '#title' => $this->t('Unsupported Currency Message'),
       '#default_value' => $message['value'],
       '#format' => $message['format'],
     );
-    $form['opus_unsupported_currencies_button'] = array(
+    $form['opus_gen_config']['opus_unsupported_currencies_button'] = array(
       '#type' => 'textfield',
-      '#title' => t('Opus Unsupported Currency button'),
+      '#title' => t('Lottey Unsupported Currency button'),
       '#description' => $this->t('Defines the Unsupported Currency LightBox Ok button'),
       '#default_value' => $config->get('opus_unsupported_currencies_button')
+    );
+    $form['gpi_lottey_gen_config'] = [
+      '#type' => 'details',
+      '#title' => t('GPI Lottery General Configurations'),
+      '#group' => 'advanced',
+    ];
+    $form['gpi_lottey_gen_config']['gpi_lottery_game_url'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Gpi Lottery Game url'),
+      '#description' => $this->t('Defines the  GPI lottery Game Url'),
+      '#default_value' => $config->get('gpi_lottery_game_url')
+    );
+    $form['gpi_lottey_gen_config']['gpi_lottery_keno_version_no'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Gpi Lottery Keno Version Number'),
+      '#description' => $this->t('Defines the  GPI lottery Keno Version Number'),
+      '#default_value' => $config->get('gpi_lottery_keno_version_no')
+    );
+    $form['gpi_lottey_gen_config']['gpi_lottery_vendor_id'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Gpi Lottery Vendor Id'),
+      '#description' => $this->t('Defines the  GPI lottery Vendor Id'),
+      '#default_value' => $config->get('gpi_lottery_vendor_id')
     );
     $form['actions'] = ['#type' => 'actions'];
 
@@ -135,6 +168,9 @@ class OpusProviderConfiguration extends ConfigFormBase {
       'opus_unsupported_currencies_message',
       'opus_unsupported_currencies_button',
       'opus_fallback_error',
+      'gpi_lottery_game_url',
+      'gpi_lottery_keno_version_no',
+      'gpi_lottery_vendor_id',
     ];
 
     foreach ($keys as $key) {
