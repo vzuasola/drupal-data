dac2c55520bbdd7f6958caf17c20da750cce8bcb
Alexander Nikko Tenepere <alexandernikko.tenepere@bayviewtechnology.com>
2018-04-30 16:23:11 +0800

    Added initial drawer entity

---
+++ web/modules/custom/webcomposer_drawer/src/Form/DrawerEntityDeleteForm.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\webcomposer_drawer\Form;
+
+use Drupal\Core\Entity\ContentEntityDeleteForm;
+
+/**
+ * Provides a form for deleting Drawer entity entities.
+ *
+ * @ingroup webcomposer_drawer
+ */
+class DrawerEntityDeleteForm extends ContentEntityDeleteForm {
+
+
+}
