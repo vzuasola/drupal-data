f97b5c67a47f32b1f50299502fce4b1e7dcf8d3d
Darryl Mabini <darryl.mabini@bayviewtechnology.com>
2018-05-08 17:41:05 +0800

    Added floating banners controller to create default callback if the view.floating_banner.manage doesn't exists yet

---
+++ web/modules/custom/webcomposer_floating_banners/src/Controller/WebcomposerFloatingBannersController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\webcomposer_floating_banners\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\views\Views;
+
+/**
+ * Controller routines for imce routes.
+ */
+class WebcomposerFloatingBannersController extends ControllerBase {
+
+  /**
+   * Default manage page
+   */
+  public function manage() {
+    $view = Views::getView('floating_banner');
+    if (isset($view) && $view->access('manage')) {
+        $view->setDisplay('manage');
+        $view->execute();
+        $markup = \Drupal::service('renderer')->renderRoot($view->render());
+    } else {
+        $markup = $this->t('Feature not available.');
+    }
+
+    return [
+      '#type' => 'markup',
+      '#markup' => $markup,
+    ];
+  }
+}
