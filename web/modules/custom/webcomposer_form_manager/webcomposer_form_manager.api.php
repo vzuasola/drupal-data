46cc837d2d0d2f57aa73bcbeda4cb62487e56a14
Alexander Nikko Tenepere <alexandernikko.tenepere@bayviewtechnology.com>
2018-05-23 05:27:56 +0000

    Fix sonar issues

--- web/modules/custom/webcomposer_form_manager/webcomposer_form_manager.api.php
+++ web/modules/custom/webcomposer_form_manager/webcomposer_form_manager.api.php
@@ -2,7 +2,6 @@
 /*
  * Use this hook to alter the validation form.
  */
-function hook_webcomposer_form_validation_alter(&$validation)
-{
+function hook_webcomposer_form_validation_alter(&$validation) {
 
 }
