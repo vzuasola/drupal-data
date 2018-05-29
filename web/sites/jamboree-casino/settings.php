3af4b61e8ae0065986cf521f18cbd653618c95b3
Ariel Alvarez <Ariel.Alvarez@bayviewtechnology.com>
2018-05-29 18:43:25 +0800

    Update settings.php

--- web/sites/jamboree-casino/settings.php
+++ web/sites/jamboree-casino/settings.php
@@ -7,6 +7,3 @@ require $app_root . '/../config/base.settings.php';
  * The product code
  */
 $settings['product'] = 'jamboree-casino';
-$settings['hash_salt'] = 'aXLXkvN11N-o78CwlfRzEYFBCkYqDI47l4orMlSZFVF6kdZgrf5qhf6_uLeDAxUedvSNjG6RDQ';
-$settings['install_profile'] = 'config_installer';
-$settings['update_free_access'] = TRUE;
