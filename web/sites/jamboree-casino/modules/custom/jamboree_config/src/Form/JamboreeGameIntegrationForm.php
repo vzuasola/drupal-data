5d69cdca623e8e08058f01c99139b769b22dcc0d
Lea Zaragoza <leapriscila.zaragoza@bayviewtechnology.com>
2018-06-01 07:43:55 +0800

    add game integration config

---
+++ web/sites/jamboree-casino/modules/custom/jamboree_config/src/Form/JamboreeGameIntegrationForm.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Drupal\jamboree_config\Form;
+
+use Drupal\webcomposer_config_schema\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Datetime\DrupalDateTime;
+
+/**
+ * My module form plugin
+ *
+ * @WebcomposerConfigPlugin(
+ *   id = "jamboree_game_integration",
+ *   route = {
+ *     "title" = "Game Integration Configuration",
+ *     "path" = "/admin/config/jamboree/game_integration_configuration",
+ *   },
+ *   menu = {
+ *     "title" = "Game Integration Configuration",
+ *     "description" = "Provides Game Integration configuration",
+ *     "parent" = "jamboree_config.jamboree_config",
+ *     "weight" = 30
+ *   },
+ * )
+ */
+class JamboreeGameIntegrationForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['jamboree_config.game_integration_configuration'];
+  }
+
+  public function form(array $form, FormStateInterface $form_state) {
+    $form['advanced'] = [
+      '#type' => 'vertical_tabs',
+      '#title' => t('Game Integration Configurations'),
+    ];
+
+    $this->sectionGameIntegration($form);
+
+    return $form;
+  }
+
+  private function sectionGameIntegration(array &$form) {
+
+    $form['game_integration'] = [
+      '#type' => 'details',
+      '#title' => t('Game Link Configuration'),
+      '#group' => 'advanced',
+    ];
+
+    $form['game_integration']['real_play_url'] = [
+      '#type' => 'textarea',
+      '#title' => t('Real Play URL'),
+      '#default_value' => $this->get('real_play_url'),
+      '#translatable' => TRUE,
+      '#required' => TRUE,
+    ];
+
+    $form['game_integration']['free_play_url'] = [
+      '#type' => 'textarea',
+      '#title' => t('Free Play URL'),
+      '#default_value' => $this->get('free_play_url'),
+      '#translatable' => TRUE,
+      '#required' => TRUE,
+    ];
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  }
