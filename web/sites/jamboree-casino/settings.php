eb30098d579fd26ad85c7e8967040f863492f0b2
Ariel Alvarez <Ariel.Alvarez@bayviewtechnology.com>
2018-06-06 15:48:50 +0800

    remove unecessary codes.

--- web/sites/jamboree-casino/settings.php
+++ web/sites/jamboree-casino/settings.php
@@ -7,15 +7,3 @@ require $app_root . '/../config/base.settings.php';
  * The product code
  */
 $settings['product'] = 'jamboree-casino';
-$databases['default']['default'] = array (
-  'database' => 'jamboree',
-  'username' => 'root',
-  'password' => 'secret',
-  'prefix' => '',
-  'host' => '10.5.0.6',
-  'port' => '3306',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-  'driver' => 'mysql',
-);
-$settings['hash_salt'] = 'pnO-VoySpsE_xaqX0H1so9WnGTmAFzNU1hOSNwnN2_fiZAYY48sS7-zpchYdtmpbQBbXLRjEBA';
-$settings['install_profile'] = 'config_installer';
