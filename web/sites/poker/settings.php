7a669d6b6ba7f2cbd0013c36ef921a0f58398e55
Aries De Los Santos <aries.delossantos@bayviewtechnology.com>
2018-06-19 13:48:45 +0800

    updated settings.php

--- web/sites/poker/settings.php
+++ web/sites/poker/settings.php
@@ -6,4 +6,14 @@ require $app_root . '/../config/base.settings.php';
 /**
  * The product code
  */
-$settings['product'] = 'product';
+$settings['product'] = 'poker';
+
+/**
+ * The main URL prefix for this site instance
+ */
+$settings['primary_site_prefix'] = 'poker';
+
+/**
+ * The front end prefix that CKEditor will append for all products
+ */
+$settings['ck_editor_inline_image_prefix'] = '/en/poker';
