d0da3bea0c464dca7edfd34ec6a99f0c1e767371
Rainer Cedric Mariano <RainerCedric.Mariano@bayviewtechnology.com>
2018-06-21 14:57:52 +0800

    Revert config changes for my account which is not intended to go on prod

--- web/sites/account/modules/custom/my_account_error_handler/src/Form/MyAccountPageNotFoundForm.php
+++ web/sites/account/modules/custom/my_account_error_handler/src/Form/MyAccountPageNotFoundForm.php
@@ -2,73 +2,163 @@
 
 namespace Drupal\my_account_error_handler\Form;
 
-use Drupal\webcomposer_config_schema\Form\FormBase;
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Path\AliasManagerInterface;
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Path\PathValidatorInterface;
+use Drupal\Core\Routing\RequestContext;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * My Account - Page Not Found.
+ * Implements the vertical tabs demo form controller.
  *
- * @WebcomposerConfigPlugin(
- *   id = "my_account_error_handler.404",
- *   route = {
- *     "title" = "Page not found Configuration",
- *     "path" = "/admin/config/my_account/page_not_found",
- *   },
- *   menu = {
- *     "title" = "Page Not Found",
- *     "description" = "My Account - Page not found configuration",
- *     "parent" = "my_account_form_profile.config",
- *   },
- * )
+ * This example demonstrates the use of \Drupal\Core\Render\Element\VerticalTabs
+ * to group input elements according category.
+ *
+ * @see \Drupal\Core\Form\FormBase
+ * @see \Drupal\Core\Form\ConfigFormBase
  */
-class MyAccountPageNotFoundForm extends FormBase {
+class MyAccountPageNotFoundForm extends ConfigFormBase
+{
 
-  /**
-   * {@inheritdoc}
-   */
-  protected function getEditableConfigNames() {
+    /**
+     * Build the form.
+     *
+     * @inheritdoc
+     */
+    public function buildForm(array $form, FormStateInterface $form_state)
+    {
 
-    return ['my_account_error_handler.404'];
-  }
+        // Get Form configuration.
+        $myAccountPageNotFoundConfig = $this->config('my_account_error_handler.404');
 
-  /**
-   * Build the form.
-   *
-   * {@inheritdoc}
-   */
-  public function form(array $form, FormStateInterface $form_state) {
+        $form['page_not_found'] = [
+            '#type' => 'vertical_tabs',
+        ];
 
-    $form['page_not_found'] = [
-      '#type' => 'vertical_tabs',
-    ];
+        $form['field_configuration'] = [
+            '#type' => 'details',
+            '#title' => 'Field Configuration',
+            '#group' => 'page_not_found',
+            '#open' => true,
+            '#tree' => true,
+        ];
 
-    $form['field_configuration'] = [
-      '#type' => 'details',
-      '#title' => 'Field Configuration',
-      '#group' => 'page_not_found',
-      '#open' => TRUE,
-      '#tree' => TRUE,
-    ];
+        $form['field_configuration']['page_not_found']['top_blurb'] = [
+            '#type' => 'textarea',
+            '#title' => t('Top Blurb'),
+            '#required' => true,
+            '#description' => $this->t('Top Blurb'),
+            '#default_value' => $myAccountPageNotFoundConfig->get('top_blurb')
+        ];
 
-    $form['field_configuration']['page_not_found']['top_blurb'] = [
-      '#type' => 'textarea',
-      '#title' => t('Top Blurb'),
-      '#required' => TRUE,
-      '#description' => $this->t('Top Blurb'),
-      '#default_value' => $this->get('top_blurb'),
-      '#translatable' => TRUE,
-    ];
+        $form['field_configuration']['page_not_found']['bottom_blurb'] = [
+            '#type' => 'textarea',
+            '#title' => t('Bottom Blurb'),
+            '#required' => true,
+            '#description' => $this->t('Bottom Blurb'),
+            '#default_value' => $myAccountPageNotFoundConfig->get('bottom_blurb')
+        ];
 
-    $form['field_configuration']['page_not_found']['bottom_blurb'] = [
-      '#type' => 'textarea',
-      '#title' => t('Bottom Blurb'),
-      '#required' => TRUE,
-      '#description' => $this->t('Bottom Blurb'),
-      '#default_value' => $this->get('bottom_blurb'),
-      '#translatable' => TRUE,
-    ];
+        $form['actions'] = ['#type' => 'actions'];
+        // Add a submit button that handles the submission of the form.
+        $form['actions']['submit'] = [
+            '#type' => 'submit',
+            '#value' => $this->t('Save'),
+        ];
+        return $form;
+    }
 
-    return $form;
-  }
+    /**
+     * Getter method for Form ID.
+     *
+     * @inheritdoc
+     */
+    public function getFormId()
+    {
+        return 'fapi_page_not_found_config';
+    }
 
+    /**
+     *
+     * @inheritdoc
+     */
+    protected function getEditableConfigNames()
+    {
+        return ['my_account_error_handler.404'];
+    }
+
+
+    /**
+     * Implements a form submit handler.
+     *
+     * @param array $form
+     *   The render array of the currently built form.
+     * @param FormStateInterface $form_state
+     *   Object describing the current state of the form.
+     */
+    public function submitForm(array &$form, FormStateInterface $form_state)
+    {
+        $configuration = $form_state->getValue('field_configuration');
+        $this->config('my_account_error_handler.404')
+            ->set('top_blurb', $configuration['page_not_found']['top_blurb'])
+            ->set('bottom_blurb', $configuration['page_not_found']['bottom_blurb'])
+            ->save();
+    }
+
+    /**
+     * The path alias manager.
+     *
+     * @var \Drupal\Core\Path\AliasManagerInterface
+     */
+    protected $aliasManager;
+
+    /**
+     * The path validator.
+     *
+     * @var \Drupal\Core\Path\PathValidatorInterface
+     */
+    protected $pathValidator;
+
+    /**
+     * The request context.
+     *
+     * @var \Drupal\Core\Routing\RequestContext
+     */
+    protected $requestContext;
+
+    /**
+     * Constructs a SiteInformationForm object.
+     *
+     * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+     *   The factory for configuration objects.
+     * @param \Drupal\Core\Path\AliasManagerInterface $alias_manager
+     *   The path alias manager.
+     * @param \Drupal\Core\Path\PathValidatorInterface $path_validator
+     *   The path validator.
+     * @param \Drupal\Core\Routing\RequestContext $request_context
+     *   The request context.
+     */
+    public function __construct(ConfigFactoryInterface $config_factory, AliasManagerInterface $alias_manager, PathValidatorInterface $path_validator, RequestContext $request_context)
+    {
+        parent::__construct($config_factory);
+
+        $this->aliasManager = $alias_manager;
+        $this->pathValidator = $path_validator;
+        $this->requestContext = $request_context;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function create(ContainerInterface $container)
+    {
+        return new static(
+            $container->get('config.factory'),
+            $container->get('path.alias_manager'),
+            $container->get('path.validator'),
+            $container->get('router.request_context')
+        );
+    }
 }
