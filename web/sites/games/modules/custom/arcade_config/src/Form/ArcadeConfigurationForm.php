a1fe66cac7f46d0682ba1644cee41596cd429166
Aries De Los Santos <aries.delossantos@bayviewtechnology.com>
2018-07-13 11:15:32 +0800

    adjustment to allow optional arcade game categories and toggling

---
+++ web/sites/games/modules/custom/arcade_config/src/Form/ArcadeConfigurationForm.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Drupal\arcade_config\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\file\Entity\File;
+
+class ArcadeConfigurationForm extends ConfigFormBase {
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['games_config.games_configuration'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'games_config.arcade_configuration_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('games_config.games_configuration');
+
+    $form['games_configuration_tab'] = array(
+      '#type' => 'vertical_tabs',
+      '#title' => t('Settings'),
+    );
+
+    $this->arcadeCategorySetting($form, $config);
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    parent::submitForm($form, $form_state);
+
+    $keys = array(
+      'arcade_category_switch'
+    );
+
+    foreach ($keys as $key) {
+      $this->config('games_config.games_configuration')->set($key, $form_state->getValue($key))->save();
+    }
+  }
+
+  private function arcadeCategorySetting(&$form, $config) {
+    $form['category_group'] = array(
+      '#type' => 'details',
+      '#title' => $this->t('Arcade Category Settings'),
+      '#collapsible' => TRUE,
+      '#group' => 'games_configuration_tab',
+    );
+
+    $form['category_group']['arcade_category_switch'] = array(
+      '#type' => 'checkbox',
+      '#title' => $this->t('Enable Arcade Categories'),
+      '#description' => $this->t('If disabled all games will be shown at once and categories will not be displayed.'),
+      '#default_value' => $config->get('arcade_category_switch'),
+      '#required' => FALSE,
+    );
+  }
+}
