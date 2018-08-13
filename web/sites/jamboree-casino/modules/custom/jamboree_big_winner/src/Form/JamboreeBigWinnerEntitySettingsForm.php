379dc43dc9ec8647d3aa1d61a7f62dbfb15e8caa
Ariel Alvarez <Ariel.Alvarez@bayviewtechnology.com>
2018-08-13 17:55:50 +0800

    jamboree big winner config files.

---
+++ web/sites/jamboree-casino/modules/custom/jamboree_big_winner/src/Form/JamboreeBigWinnerEntitySettingsForm.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\jamboree_big_winner\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class JamboreeBigWinnerEntitySettingsForm.
+ *
+ * @ingroup jamboree_big_winner
+ */
+class JamboreeBigWinnerEntitySettingsForm extends FormBase {
+
+  /**
+   * Returns a unique string identifying the form.
+   *
+   * @return string
+   *   The unique string identifying the form.
+   */
+  public function getFormId() {
+    return 'jamboreebigwinnerentity_settings';
+  }
+
+  /**
+   * Form submission handler.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Empty implementation of the abstract submit class.
+  }
+
+  /**
+   * Defines the settings form for Jamboree big winner entity entities.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   *
+   * @return array
+   *   Form definition array.
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['jamboreebigwinnerentity_settings']['#markup'] = 'Settings form for Jamboree big winner entity entities. Manage field settings here.';
+    return $form;
+  }
+
+}
