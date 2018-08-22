d9175ca300bbc8c06ebf79eca11405cdf7d71f7d
Aries De Los Santos <aries.delossantos@bayviewtechnology.com>
2018-08-22 14:13:25 +0800

    fixed indentions

--- web/sites/poker/modules/custom/poker_game_offers/src/Form/GameOffersConfigurationForm.php
+++ web/sites/poker/modules/custom/poker_game_offers/src/Form/GameOffersConfigurationForm.php
@@ -37,12 +37,12 @@ class GameOffersConfigurationForm extends FormBase
     public function form(array $form, FormStateInterface $form_state)
     {
         $form['title'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Page Title'),
-      '#default_value' => $this->get('title'),
-      '#translatable' => true,
-      '#required' => true
-    ];
+          '#type' => 'textfield',
+          '#title' => $this->t('Page Title'),
+          '#default_value' => $this->get('title'),
+          '#translatable' => true,
+          '#required' => true
+        ];
 
         $this->casinoGames($form);
         $this->pokerGames($form);
@@ -54,147 +54,147 @@ class GameOffersConfigurationForm extends FormBase
     private function casinoGames(&$form)
     {
         $form['casino'] = [
-      '#type' => 'details',
-      '#title' => $this->t('Casino Game Offers'),
-      '#collapsible' => true,
-      '#open' => true,
-    ];
+          '#type' => 'details',
+          '#title' => $this->t('Casino Game Offers'),
+          '#collapsible' => true,
+          '#open' => true,
+        ];
 
         $form['casino']['casino_label'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Casino Tab Label'),
-      '#default_value' => $this->get('casino_label'),
-      '#translatable' => true,
-      '#required' => true
-    ];
+          '#type' => 'textfield',
+          '#title' => $this->t('Casino Tab Label'),
+          '#default_value' => $this->get('casino_label'),
+          '#translatable' => true,
+          '#required' => true
+        ];
 
         $form['casino']['file_image_casino_icon'] = [
-      '#type' => 'managed_file',
-      '#title' => t('Tab Default Icon'),
-      '#description' => t('Upload a file, allowed extensions: jpg, jpeg, png, gif'),
-      '#upload_location' => 'public://',
-      '#upload_validators' => [
-        'file_validate_extensions' => ['png jpg jpeg gif'],
-      ],
-      '#default_value' => $this->get('file_image_casino_icon'),
-      '#required' => true,
-    ];
+          '#type' => 'managed_file',
+          '#title' => t('Tab Default Icon'),
+          '#description' => t('Upload a file, allowed extensions: jpg, jpeg, png, gif'),
+          '#upload_location' => 'public://',
+          '#upload_validators' => [
+            'file_validate_extensions' => ['png jpg jpeg gif'],
+          ],
+          '#default_value' => $this->get('file_image_casino_icon'),
+          '#required' => true,
+        ];
 
         $form['casino']['file_image_casino_icon_hover'] = [
-      '#type' => 'managed_file',
-      '#title' => t('Tab Hover/Active Icon'),
-      '#description' => t('Upload a file, allowed extensions: jpg, jpeg, png, gif'),
-      '#upload_location' => 'public://',
-      '#upload_validators' => [
-        'file_validate_extensions' => ['png jpg jpeg gif'],
-      ],
-      '#default_value' => $this->get('file_image_casino_icon_hover'),
-      '#required' => true,
-    ];
+          '#type' => 'managed_file',
+          '#title' => t('Tab Hover/Active Icon'),
+          '#description' => t('Upload a file, allowed extensions: jpg, jpeg, png, gif'),
+          '#upload_location' => 'public://',
+          '#upload_validators' => [
+            'file_validate_extensions' => ['png jpg jpeg gif'],
+          ],
+          '#default_value' => $this->get('file_image_casino_icon_hover'),
+          '#required' => true,
+        ];
     }
 
     private function pokerGames(&$form)
     {
         $form['poker'] = [
-      '#type' => 'details',
-      '#title' => $this->t('Poker Game Offers'),
-      '#collapsible' => true,
-      '#open' => true,
-    ];
+          '#type' => 'details',
+          '#title' => $this->t('Poker Game Offers'),
+          '#collapsible' => true,
+          '#open' => true,
+        ];
 
         $form['poker']['poker_label'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Poker Tab Label'),
-      '#default_value' => $this->get('poker_label'),
-      '#translatable' => true,
-      '#required' => true
-    ];
+          '#type' => 'textfield',
+          '#title' => $this->t('Poker Tab Label'),
+          '#default_value' => $this->get('poker_label'),
+          '#translatable' => true,
+          '#required' => true
+        ];
 
         $form['poker']['file_image_poker_icon'] = [
-      '#type' => 'managed_file',
-      '#title' => t('Tab Default Icon'),
-      '#description' => t('Upload a file, allowed extensions: jpg, jpeg, png, gif'),
-      '#upload_location' => 'public://',
-      '#upload_validators' => [
-        'file_validate_extensions' => ['png jpg jpeg gif'],
-      ],
-      '#default_value' => $this->get('file_image_poker_icon'),
-      '#required' => true,
-    ];
+          '#type' => 'managed_file',
+          '#title' => t('Tab Default Icon'),
+          '#description' => t('Upload a file, allowed extensions: jpg, jpeg, png, gif'),
+          '#upload_location' => 'public://',
+          '#upload_validators' => [
+            'file_validate_extensions' => ['png jpg jpeg gif'],
+          ],
+          '#default_value' => $this->get('file_image_poker_icon'),
+          '#required' => true,
+        ];
 
         $form['poker']['file_image_poker_icon_hover'] = [
-      '#type' => 'managed_file',
-      '#title' => t('Tab Hover/Active Icon'),
-      '#description' => t('Upload a file, allowed extensions: jpg, jpeg, png, gif'),
-      '#upload_location' => 'public://',
-      '#upload_validators' => [
-        'file_validate_extensions' => ['png jpg jpeg gif'],
-      ],
-      '#default_value' => $this->get('file_image_poker_icon_hover'),
-      '#required' => true,
-    ];
+          '#type' => 'managed_file',
+          '#title' => t('Tab Hover/Active Icon'),
+          '#description' => t('Upload a file, allowed extensions: jpg, jpeg, png, gif'),
+          '#upload_location' => 'public://',
+          '#upload_validators' => [
+            'file_validate_extensions' => ['png jpg jpeg gif'],
+          ],
+          '#default_value' => $this->get('file_image_poker_icon_hover'),
+          '#required' => true,
+        ];
     }
 
     private function common(&$form)
     {
         $form['common'] = [
-      '#type' => 'details',
-      '#title' => $this->t('Common'),
-      '#collapsible' => true,
-      '#open' => true,
-    ];
+          '#type' => 'details',
+          '#title' => $this->t('Common'),
+          '#collapsible' => true,
+          '#open' => true,
+        ];
 
         $form['common']['download_label'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Download Button Label'),
-      '#default_value' => $this->get('download_label'),
-      '#translatable' => true,
-      '#required' => true
-    ];
+          '#type' => 'textfield',
+          '#title' => $this->t('Download Button Label'),
+          '#default_value' => $this->get('download_label'),
+          '#translatable' => true,
+          '#required' => true
+        ];
 
         $form['common']['download_link'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Download Button Link'),
-      '#default_value' => $this->get('download_link'),
-      '#translatable' => true,
-      '#required' => true
-    ];
+          '#type' => 'textfield',
+          '#title' => $this->t('Download Button Link'),
+          '#default_value' => $this->get('download_link'),
+          '#translatable' => true,
+          '#required' => true
+        ];
 
         $form['common']['play_label'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Play Button Label'),
-      '#default_value' => $this->get('play_label'),
-      '#translatable' => true,
-      '#required' => true
-    ];
+          '#type' => 'textfield',
+          '#title' => $this->t('Play Button Label'),
+          '#default_value' => $this->get('play_label'),
+          '#translatable' => true,
+          '#required' => true
+        ];
 
         $form['common']['info_label'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Game Info Label'),
-      '#default_value' => $this->get('info_label'),
-      '#translatable' => true,
-      '#required' => true
-    ];
+          '#type' => 'textfield',
+          '#title' => $this->t('Game Info Label'),
+          '#default_value' => $this->get('info_label'),
+          '#translatable' => true,
+          '#required' => true
+        ];
 
         $form['common']['promo_link'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Promo Button Link'),
-      '#default_value' => $this->get('promo_link'),
-      '#translatable' => true,
-      '#required' => true
-    ];
+          '#type' => 'textfield',
+          '#title' => $this->t('Promo Button Link'),
+          '#default_value' => $this->get('promo_link'),
+          '#translatable' => true,
+          '#required' => true
+        ];
 
         $form['common']['promo_link_target'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Promo Button Link Target'),
-      '#default_value' => $this->get('promo_link_target'),
-      '#translatable' => true,
-      '#required' => true,
-      '#options' => [
-        '_blank' => 'New Tab',
-        '_self' => 'Same Window',
-        'window' => 'New Window'
-      ]
-    ];
+          '#type' => 'select',
+          '#title' => $this->t('Promo Button Link Target'),
+          '#default_value' => $this->get('promo_link_target'),
+          '#translatable' => true,
+          '#required' => true,
+          '#options' => [
+            '_blank' => 'New Tab',
+            '_self' => 'Same Window',
+            'window' => 'New Window'
+          ]
+        ];
     }
 }
