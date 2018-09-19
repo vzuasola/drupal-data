ea60f2dd3427dd75cf075632d23fac2b31923ef5
Ronald Duque <ronald.duque@bayviewtechnology.com>
2018-09-19 09:39:08 +0000

    Revert "Merge branch 'feature-VIRTUALS-710-fixed-audit-log-fixed' into 'sprint-21-product-integration'"
    This reverts merge request !3132

--- web/modules/custom/webcomposer_games/src/Form/ICoreGamesIntegrationConfiguration.php
+++ web/modules/custom/webcomposer_games/src/Form/ICoreGamesIntegrationConfiguration.php
@@ -8,7 +8,7 @@ use Drupal\Core\Form\FormStateInterface;
  * ICore Games Integration Configuration Form
  *
  * @WebcomposerConfigPlugin(
- *   id = "icore_config_form",
+ *   id = "gpi_config_form",
  *   route = {
  *     "title" = "Webcomposer Games Configuration",
  *     "path" = "/admin/config/webcomposer/games/icore",
