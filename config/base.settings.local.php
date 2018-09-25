0cee5c1a08b3338f6d95c5f26eeb3f7e76f7e084
Alexander Nikko Tenepere <alexandernikko.tenepere@bayviewtechnology.com>
2018-09-25 18:37:24 +0800

    Added Redis params for local

--- config/base.settings.local.php
+++ config/base.settings.local.php
@@ -5,14 +5,29 @@
  * Drupal site-specific configuration file for development
  */
 
+$clients = 'tcp://10.5.0.7:6379';
+$options = [
+  'parameters' => ['database' => 1],
+];
+
 /**
  * Redis host
  */
 $settings['webcomposer_cache']['redis'] = [
-  'clients' => 'tcp://10.5.0.7:6379',
-  'options' => [
-      'parameters' => ['database' => 1],
-  ],
+  'clients' => $clients,
+  'options' => $options,
+];
+
+$settings['redis.connection']['interface'] = 'Predis';
+$settings['redis.connection']['host'] = $clients;
+$settings['redis.connection']['options'] = $options;
+
+/**
+ * Redis backend
+ */
+$settings['redis.connection']['host'] = 'tcp://10.5.0.7:6379';
+$settings['redis.connection']['options'] = [
+  'parameters' => ['database' => 1],
 ];
 
 /**
