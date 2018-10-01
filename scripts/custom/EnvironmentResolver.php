420b49e18139ea72c2b10458865c53b198089a27
Alexander Nikko Tenepere <alexandernikko.tenepere@bayviewtechnology.com>
2018-10-01 11:05:35 +0800

    Added status message based on environment

---
+++ scripts/custom/EnvironmentResolver.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @file
+ * Contains \DrupalProject\custom\ProductHandler.
+ */
+
+namespace DrupalProject\custom;
+
+use Symfony\Component\HttpFoundation\Request;
+
+class EnvironmentResolver {
+  public static function getEnvironment() {
+    return self::getEnvironmentFromRequest(\Drupal::request());
+  }
+
+  public static function getEnvironmentFromRequest(Request $request) {
+    $environment = 'LOCAL';
+
+    $hostname = $request->getHttpHost();
+    $hostname = 'prd-mobile-entrypage.elysium-dfbt.com';
+
+    if (preg_match('/(.*?)-/', $hostname, $matches)) {
+      list(, $env) = $matches;
+
+      $environment = strtoupper($env);
+    }
+
+    return $environment;
+  }
+}
