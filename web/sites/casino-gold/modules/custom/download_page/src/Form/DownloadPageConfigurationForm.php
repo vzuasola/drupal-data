7d611eba393cdea27bfda92e7f31ce15d915692f
Aries De Los Santos <aries.delossantos@bayviewtechnology.com>
2018-10-29 15:57:37 +0800

    added download page module

---
+++ web/sites/casino-gold/modules/custom/download_page/src/Form/DownloadPageConfigurationForm.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Drupal\download_page\Form;
+
+use Drupal\webcomposer_config_schema\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Download page form plugin
+ *
+ * @WebcomposerConfigPlugin(
+ *   id = "download_page",
+ *   route = {
+ *     "title" = "Download Page Configuration Form",
+ *     "path" = "/admin/config/download_page/settings",
+ *   },
+ *   menu = {
+ *     "title" = "Download Page Configuration",
+ *     "description" = "Provides configuration for Download Page",
+ *     "parent" = "download_page.list",
+ *     "weight" = -5
+ *   },
+ * )
+ */
+class DownloadPageConfigurationForm extends FormBase {
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['poker_config.download_page'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function form(array $form, FormStateInterface $form_state) {
+    $form['title'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Page Title'),
+      '#default_value' => $this->get('title'),
+      '#translatable' => TRUE,
+    ];
+
+    $form['client_label'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Game Client Label'),
+      '#default_value' => $this->get('client_label'),
+      '#translatable' => TRUE,
+    ];
+
+    $form['mobile_label'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Mobile Label'),
+      '#default_value' => $this->get('mobile_label'),
+      '#translatable' => TRUE,
+    ];
+
+    return $form;
+  }
+}
