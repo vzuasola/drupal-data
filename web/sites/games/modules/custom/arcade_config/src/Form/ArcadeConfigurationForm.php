be13c30cfbca9f8c0d04cdef8c5dbfadd2425f07
Aldrin Nikolai Tomas <AldrinNikolai.Tomas@bayviewtechnology.com>
2018-11-27 19:15:49 +0800

    Added reg link overrides Migrated ArcadeConfigForm to config schema

--- web/sites/games/modules/custom/arcade_config/src/Form/ArcadeConfigurationForm.php
+++ web/sites/games/modules/custom/arcade_config/src/Form/ArcadeConfigurationForm.php
@@ -1,12 +1,27 @@
 <?php
-
 namespace Drupal\arcade_config\Form;
 
-use Drupal\Core\Form\ConfigFormBase;
+use Drupal\webcomposer_config_schema\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\file\Entity\File;
 
-class ArcadeConfigurationForm extends ConfigFormBase {
+/**
+ * Arcade custom config form plugin
+ *
+ * @WebcomposerConfigPlugin(
+ *   id = "arcade_config_form",
+ *   route = {
+ *     "title" = "Arcade Custom Configuration",
+ *     "path" = "/admin/arcade/config",
+ *   },
+ *   menu = {
+ *     "title" = "Arcade Custom Configuration",
+ *     "description" = "Configure custom arcade configuration",
+ *     "parent" = "arcade_config.admin_settings",
+ *     "weight" = -5
+ *   },
+ * )
+ */
+class ArcadeConfigurationForm extends FormBase {
   /**
    * {@inheritdoc}
    */
@@ -17,42 +32,19 @@ class ArcadeConfigurationForm extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
-  public function getFormId() {
-    return 'games_config.arcade_configuration_form';
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function buildForm(array $form, FormStateInterface $form_state) {
-    $config = $this->config('games_config.arcade_configuration');
-
+  public function form(array $form, FormStateInterface $form_state) {
     $form['arcade_configuration_tab'] = array(
       '#type' => 'vertical_tabs',
       '#title' => t('Settings'),
     );
 
-    $this->arcadeCategorySetting($form, $config);
+    $this->arcadeCategorySetting($form);
+    $this->arcadeHeaderSettingsOverride($form);
 
-    return parent::buildForm($form, $form_state);
+    return $form;
   }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-    parent::submitForm($form, $form_state);
-
-    $keys = array(
-      'arcade_category_switch'
-    );
-
-    foreach ($keys as $key) {
-      $this->config('games_config.arcade_configuration')->set($key, $form_state->getValue($key))->save();
-    }
-  }
-
-  private function arcadeCategorySetting(&$form, $config) {
+  private function arcadeCategorySetting(&$form) {
     $form['category_group'] = array(
       '#type' => 'details',
       '#title' => $this->t('Arcade Category Settings'),
@@ -64,8 +56,46 @@ class ArcadeConfigurationForm extends ConfigFormBase {
       '#type' => 'checkbox',
       '#title' => $this->t('Enable Arcade Categories'),
       '#description' => $this->t('If disabled all games will be shown at once and categories will not be displayed.'),
-      '#default_value' => $config->get('arcade_category_switch'),
+      '#default_value' => $this->get('arcade_category_switch'),
       '#required' => FALSE,
+    );
+  }
+
+  private function arcadeHeaderSettingsOverride(&$form) {
+    $form['header_config_group'] = array(
+      '#type' => 'details',
+      '#title' => $this->t('Header Settings Override'),
+      '#description' => $this->t(
+        '<br/><i><b>NOTE:</b>All empty fields will use the original field under webcomposer header configurations</i>'
+      ),
+      '#collapsible' => TRUE,
+      '#group' => 'arcade_configuration_tab',
+    );
+
+    $form['header_config_group']['registration'] = [
+      '#type' => 'details',
+      '#title' => $this->t('Registration'),
+      '#description' => $this->t('Registration header config override for arcade.'),
+      '#collapsible' => TRUE,
+      '#open' => TRUE,
+    ];
+
+    $form['header_config_group']['registration']['arcade_header_override_reg_label'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Join Now Button Text'),
+      '#description' => $this->t('This field will be used as the join now link for arcade site.'),
+      '#default_value' => $this->get('arcade_header_override_reg_label'),
+      '#required' => FALSE,
+      '#translatable' => true
+    );
+
+    $form['header_config_group']['registration']['arcade_header_override_reg_link'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Registration Link'),
+      '#description' => $this->t('This field will be used as the join now link for arcade site.'),
+      '#default_value' => $this->get('arcade_header_override_reg_link'),
+      '#required' => FALSE,
+      '#translatable' => true
     );
   }
 }
