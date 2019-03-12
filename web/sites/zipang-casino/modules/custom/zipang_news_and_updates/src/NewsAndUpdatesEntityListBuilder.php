8ade94aa50fc3489af26126dd5cd393eaca45909
Mary Jane Lorenzo <mjlorenzo@bti00008584.ph.esl-asia.com>
2019-03-12 09:44:44 +0800

    news and updates config

---
+++ web/sites/zipang-casino/modules/custom/zipang_news_and_updates/src/NewsAndUpdatesEntityListBuilder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\zipang_news_and_updates;
+
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityListBuilder;
+use Drupal\Core\Link;
+
+/**
+ * Defines a class to build a listing of News and updates entity entities.
+ *
+ * @ingroup zipang_news_and_updates
+ */
+class NewsAndUpdatesEntityListBuilder extends EntityListBuilder {
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildHeader() {
+    $header['name'] = $this->t('Name');
+    return $header + parent::buildHeader();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildRow(EntityInterface $entity) {
+    /* @var $entity \Drupal\zipang_news_and_updates\Entity\NewsAndUpdatesEntity */
+    $row['name'] = Link::createFromRoute(
+      $entity->label(),
+      'entity.news_and_updates_entity.edit_form',
+      ['news_and_updates_entity' => $entity->id()]
+    );
+    return $row + parent::buildRow($entity);
+  }
+
+}
