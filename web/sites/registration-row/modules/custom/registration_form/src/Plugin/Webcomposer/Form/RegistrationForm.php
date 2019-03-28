65d21bd9fb793d9a1497dee984e7776e0e0f55df
Melvin Reyes <melvin.reyes@bayviewtechnology.com>
2019-03-28 13:40:35 +0800

    added default values

--- web/sites/registration-row/modules/custom/registration_form/src/Plugin/Webcomposer/Form/RegistrationForm.php
+++ web/sites/registration-row/modules/custom/registration_form/src/Plugin/Webcomposer/Form/RegistrationForm.php
@@ -61,11 +61,13 @@ class RegistrationForm extends WebcomposerFormBase implements WebcomposerFormInt
             '#title' => 'Username label',
             '#type' => 'textfield',
             '#description' => 'label for Username field',
+            '#default_value' => 'Username',
           ],
           'placeholder' => [
             '#title' => 'Username placeholder label',
             '#type' => 'textfield',
             '#description' => 'label for Username field placeholder',
+            '#default_value' => 'Username',
           ],
           'annotation' => [
             '#title' => 'Username Annotation text',
@@ -83,11 +85,13 @@ class RegistrationForm extends WebcomposerFormBase implements WebcomposerFormInt
             '#title' => 'Password label',
             '#type' => 'textfield',
             '#description' => 'The Label for Password field',
+            '#default_value' => 'Password',
           ],
           'placeholder' => [
             '#title' => 'Password placeholder label',
             '#type' => 'textfield',
             '#description' => 'label for Password field placeholder',
+            '#default_value' => 'Password',
           ],
           'annotation' => [
             '#title' => 'Password Annotation text',
@@ -118,11 +122,13 @@ class RegistrationForm extends WebcomposerFormBase implements WebcomposerFormInt
             '#title' => 'Confirm Password label',
             '#type' => 'textfield',
             '#description' => 'The Label for Password field',
+            '#default_value' => 'Confirm Password',
           ],
           'placeholder' => [
             '#title' => 'Confirm Password placeholder label',
             '#type' => 'textfield',
             '#description' => 'label for Password field placeholder',
+            '#default_value' => 'Confirm Password',
           ],
           'annotation' => [
             '#title' => 'Confirm Password Annotation text',
@@ -132,25 +138,12 @@ class RegistrationForm extends WebcomposerFormBase implements WebcomposerFormInt
         ],
       ],
 
-      'confirm_password_meter' => [
-        'name' => 'Password Meter',
-        'type' => 'markup',
-        'settings' => [
-          'markup' => [
-            '#title' => 'Confirm Password meter markup',
-            '#type' => 'textarea',
-            '#description' => 'HTML markup for password meter.',
-            '#default_value' => '<div class="password_meter_wrapper password-meter-hidden grid mb-0"></div>',
-          ],
-        ],
-      ],
-
       'breakline_1' => [
-        'name' => 'Break Line Markup',
+        'name' => 'Break Line Mark',
         'type' => 'markup',
         'settings' => [
           'markup' => [
-            '#title' => 'Horizontal Line Markup',
+            '#title' => 'Horizonta',
             '#type' => 'textarea',
             '#description' => 'A Markup text breakline for registration form',
             '#default_value' => '<hr>',
@@ -179,11 +172,13 @@ class RegistrationForm extends WebcomposerFormBase implements WebcomposerFormInt
             '#title' => 'First Name Label',
             '#type' => 'textfield',
             '#description' => 'The label for the First Name field',
+            '#default_value' => 'First Name',
           ],
           'placeholder' => [
             '#title' => 'First name placeholder label',
             '#type' => 'textfield',
             '#description' => 'label for First name field placeholder',
+            '#default_value' => 'First Name',
           ],
           'annotation' => [
             '#title' => 'First Name Annotation text',
@@ -201,11 +196,13 @@ class RegistrationForm extends WebcomposerFormBase implements WebcomposerFormInt
             '#title' => 'Last Name Label',
             '#type' => 'textfield',
             '#description' => 'The label for the Last Name field',
+            '#default_value' => 'Last Name',
           ],
           'placeholder' => [
             '#title' => 'Last name placeholder label',
             '#type' => 'textfield',
             '#description' => 'label for Last name field placeholder',
+            '#default_value' => 'Last Name',
           ],
           'annotation' => [
             '#title' => 'Last Name Annotation text',
@@ -223,16 +220,19 @@ class RegistrationForm extends WebcomposerFormBase implements WebcomposerFormInt
             '#title' => 'Date of Birth Label',
             '#type' => 'textfield',
             '#description' => 'The Label for the Birthdate field',
+            '#default_value' => 'Date of Birth',
           ],
           'placeholder' => [
             '#title' => 'Birthdate placeholder label',
             '#type' => 'textfield',
             '#description' => 'label for Birthdate field placeholder',
+            '#default_value' => 'DD/MM/YYYY',
           ],
           'date-format' => [
             '#title' => 'Birthdate format',
             '#type' => 'textfield',
             '#description' => 'Date Format for Birthdate',
+            '#default_value' => 'DD/MM/YYYY',
           ],
           'annotation' => [
             '#title' => 'Birthdate Annotation text',
@@ -250,6 +250,7 @@ class RegistrationForm extends WebcomposerFormBase implements WebcomposerFormInt
             '#title' => 'Security Question 1 Label',
             '#type' => 'textfield',
             '#description' => 'The dLabel for the Currency field',
+            '#default_value' => 'Security Question 1',
           ],
           'placeholder' => [
             '#title' => 'Choose a Security Question 1',
@@ -261,15 +262,16 @@ class RegistrationForm extends WebcomposerFormBase implements WebcomposerFormInt
             '#title' => 'Group Name',
             '#type' => 'textfield',
             '#description' => 'Group name for Security Answer 1',
+            '#default_value' => 'security-question',
           ],
           'choices' => [
             '#title' => 'Security Question 1 Choices',
             '#type' => 'textarea',
             '#description' => 'Provide a pipe separated key value pair. <br> <small>Example key|My Value</small>',
             '#default_value' => implode(PHP_EOL, [
-              '117|RMB/CNY',
-              '2|USD',
-              '1|EUR',
+              '1|What is your mother’s maiden name?',
+              '2|What is your father’s first name?',
+              '3|What is the model of your first car?',
             ]),
           ],
         ],
@@ -283,11 +285,13 @@ class RegistrationForm extends WebcomposerFormBase implements WebcomposerFormInt
             '#title' => 'Security Answer 1 label',
             '#type' => 'textfield',
             '#description' => 'label for Security Answer 1 field',
+            '#default_value' => 'Security Answer 1',
           ],
           'placeholder' => [
             '#title' => 'Security Answer 1 placeholder label',
             '#type' => 'textfield',
             '#description' => 'label for Security Answer 1 field placeholder',
+            '#default_value' => 'Security Answer 1',
           ],
           'annotation' => [
             '#title' => 'Security Answer 1 Annotation text',
@@ -305,6 +309,7 @@ class RegistrationForm extends WebcomposerFormBase implements WebcomposerFormInt
             '#title' => 'Security Question 2 Label',
             '#type' => 'textfield',
             '#description' => 'The dLabel for the Currency field',
+            '#default_value' => 'Security Question 2',
           ],
           'placeholder' => [
             '#title' => 'Choose a Security Question 2',
@@ -316,15 +321,16 @@ class RegistrationForm extends WebcomposerFormBase implements WebcomposerFormInt
             '#title' => 'Group Name',
             '#type' => 'textfield',
             '#description' => 'Group name for Security Answer 1',
+            '#default_value' => 'security-question',
           ],
           'choices' => [
             '#title' => 'Security Question 2 Choices',
             '#type' => 'textarea',
             '#description' => 'Provide a pipe separated key value pair. <br> <small>Example key|My Value</small>',
             '#default_value' => implode(PHP_EOL, [
-              '117|RMB/CNY',
-              '2|USD',
-              '1|EUR',
+              '1|What is your mother’s maiden name?',
+              '2|What is your father’s first name?',
+              '3|What is the model of your first car?',
             ]),
           ],
         ],
@@ -338,11 +344,13 @@ class RegistrationForm extends WebcomposerFormBase implements WebcomposerFormInt
             '#title' => 'Security Answer 2 label',
             '#type' => 'textfield',
             '#description' => 'label for Security Answer 2 field',
+            '#default_value' => 'Security Answer 2',
           ],
           'placeholder' => [
             '#title' => 'Security Answer 2 placeholder label',
             '#type' => 'textfield',
             '#description' => 'label for Security Answer 2 field placeholder',
+            '#default_value' => 'Security Answer 2',
           ],
           'annotation' => [
             '#title' => 'Security Answer 2 Annotation text',
@@ -400,7 +408,7 @@ class RegistrationForm extends WebcomposerFormBase implements WebcomposerFormInt
             '#title' => 'Accept Terms Text',
             '#type' => 'textarea',
             '#description' => 'A Markup text for accept terms and conditions beside the submit button',
-            '#default_value' => 'I am at least 18 years old and have read and accept the Terms and Condition',
+            '#default_value' => 'I have read and accept the Terms and Condition',
           ],
         ],
       ],
@@ -413,7 +421,7 @@ class RegistrationForm extends WebcomposerFormBase implements WebcomposerFormInt
             '#title' => 'Legal Age Text',
             '#type' => 'textarea',
             '#description' => 'A Markup text for accept terms and conditions beside the submit button',
-            '#default_value' => 'I am at least 18 years old and have read and accept the Terms and Condition',
+            '#default_value' => 'I am at least 18 years old',
           ],
         ],
       ],
