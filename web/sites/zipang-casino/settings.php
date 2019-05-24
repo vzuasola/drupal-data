63a544ebe23e823dbdbb100eab9a87f6ad41b234
John Benedict Quitoriano <johnbenedict.quitoriano@bayviewtechnology.com>
2019-05-24 02:06:41 +0000

    Update settings.php

--- web/sites/zipang-casino/settings.php
+++ web/sites/zipang-casino/settings.php
@@ -6,15 +6,4 @@ require $app_root . '/../config/base.settings.php';
 /**
  * The product code
  */
-$settings['product'] = 'zipang-casino';$databases['default']['default'] = array (
-  'database' => 'zipang',
-  'username' => 'root',
-  'password' => 'secret',
-  'prefix' => '',
-  'host' => '10.5.0.6',
-  'port' => '3306',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-  'driver' => 'mysql',
-);
-$settings['hash_salt'] = 'xXouPNrZhKMP20MCF7NznDduHNMYILVMrpxHTLETiXjtwJMbhvc_b8mqQu3Qd-zD4TsdfwGebg';
-$settings['install_profile'] = 'config_installer';
+$settings['product'] = 'zipang-casino';
