94cd9ab83628ac678a05168b8b2d76d24030da82
Alexander Nikko Tenepere <alexandernikko.tenepere@bayviewtechnology.com>
2019-05-28 16:28:33 +0800

    Added dynamic sites.php

--- web/sites/sites.php
+++ web/sites/sites.php
@@ -5,6 +5,11 @@
  * Configuration file for multi-site support and directory aliasing feature.
  */
 
+// dynamic sites.php values via environment variables
+if (isset($_SERVER['DRUPAL_SITES_DOMAIN']) && isset($_SERVER['DRUPAL_SITES_MULTISITE'])) {
+  $sites[$_SERVER['DRUPAL_SITES_DOMAIN']] = $_SERVER['DRUPAL_SITES_MULTISITE'];
+}
+
 // Webcomposer Demo Instances
 $sites['localhost'] = 'demo';
 $sites['demo.drupal.dev'] = 'demo';
