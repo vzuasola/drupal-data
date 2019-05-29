94331af951e4fb08c1e7eb15e4790684a9396cfe
Jerome Michael <jerome.michael@bayviewtechnology.com>
2019-05-29 09:49:30 +0800

    From ALS Config to General Config, added selection tab for Footer config

--- web/sites/dafa-ghana/modules/custom/ghana_config/src/Form/GhanaALSConfigForm.php
+++ web/sites/dafa-ghana/modules/custom/ghana_config/src/Form/GhanaALSConfigForm.php
@@ -6,19 +6,19 @@ use Drupal\webcomposer_config_schema\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
- * My module form plugin
+ * General Configuration Plugin
  *
  * @WebcomposerConfigPlugin(
  *   id = "ghana_als_config_form",
  *   route = {
- *     "title" = "ALS Configuration",
+ *     "title" = "General Configuration",
  *     "path" = "/admin/config/ghana/als_configuration",
  *   },
  *   menu = {
- *     "title" = "ALS Configuration",
- *     "description" = "Provides ALS configuration",
+ *     "title" = "General Configuration",
+ *     "description" = "Provides General configuration",
  *     "parent" = "ghana_config.list",
- *     "weight" = 30
+ *     "weight" = 10
  *   },
  * )
  */
@@ -34,42 +34,62 @@ class GhanaALSConfigForm extends FormBase {
    * {@inheritdoc}
    */
   public function form(array $form, FormStateInterface $form_state) {
-    $form['integration'] = [
+    $form['advanced'] = [
+        '#type' => 'vertical_tabs',
+        '#title' => t('General Configuration'),
+    ];
+
+    $this->sectionPageSetting($form);
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  private function sectionPageSetting(array &$form){
+    $form['als_configuration_setting'] = [
+      '#type' => 'details',
+      '#title' => t('ALS Configuration'),
+      '#group' => 'advanced',
+    ];
+
+    $form['als_configuration_setting']['integration'] = [
       '#type' => 'details',
       '#title' => $this->t('Integration'),
       '#collapsible' => TRUE,
       '#open' => TRUE,
     ];
 
-    $form['integration']['als_url'] = [
+    $form['als_configuration_setting']['integration']['als_url'] = [
       '#type' => 'textfield',
       '#title' => $this->t('ALS URL'),
       '#default_value' => $this->get('als_url'),
       '#translatable' => TRUE,
     ];
 
-    $form['integration']['als_cookie_url_pre'] = [
+    $form['als_configuration_setting']['integration']['als_cookie_url_pre'] = [
       '#type' => 'textarea',
       '#title' => $this->t('Cookie URLs (Pre login)'),
       '#default_value' => $this->get('als_cookie_url_pre'),
       '#translatable' => TRUE,
     ];
 
-    $form['integration']['als_cookie_url_post'] = [
+    $form['als_configuration_setting']['integration']['als_cookie_url_post'] = [
       '#type' => 'textarea',
       '#title' => $this->t('Cookie URLs (Post login)'),
       '#default_value' => $this->get('als_cookie_url_post'),
       '#translatable' => TRUE,
     ];
 
-    $form['domains'] = [
+    $form['als_configuration_setting']['domains'] = [
       '#type' => 'details',
       '#title' => $this->t('Domain'),
       '#collapsible' => TRUE,
       '#open' => TRUE,
     ];
 
-    $form['domains']['als_enable_domain'] = [
+    $form['als_configuration_setting']['domains']['als_enable_domain'] = [
       '#type' => 'checkbox',
       '#title' => $this->t('Domain Mapping'),
       '#default_value' => $this->get('als_enable_domain'),
@@ -77,6 +97,26 @@ class GhanaALSConfigForm extends FormBase {
       '#translatable' => TRUE,
     ];
 
-    return $form;
+    $form['footer_configuration_setting'] = [
+      '#type' => 'details',
+      '#title' => t('Footer Setting'),
+      '#group' => 'advanced',
+    ];
+
+    $form['footer_configuration_setting']['ghana_footer_title'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Title'),
+      '#default_value' => $this->get('ghana_footer_title'),
+      '#translatable' => TRUE,
+    ];
+
+    $c = $this->get('ghana_footer_desc');
+    $form['footer_configuration_setting']['ghana_footer_desc'] = [
+      '#type' => 'text_format',
+      '#title' => $this->t('Description'),
+      '#default_value' => $c['value'],
+      '#format' => $c['format'],
+      '#translatable' => TRUE,
+    ];
   }
 }
