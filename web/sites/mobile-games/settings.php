59ac2d3533a770819e40df33c4f582fa2c28d939
Stephanie Aviles <stephanie.aviles@bayviewtechnology.com>
2019-06-21 15:28:50 +0800

    updated settings

--- web/sites/mobile-games/settings.php
+++ web/sites/mobile-games/settings.php
@@ -13,3 +13,8 @@ if (isset($_SERVER['REDIS_SERVER']) && isset($_SERVER['REDIS_SERVICE'])) {
     $settings['cache']['default'] = 'cache.backend.redis';
     $settings['cache_prefix'] = "drupal.cache.$product";
 }
+
+$settings['redis_perm_ttl'] = "3 hours";
+$settings['redis_perm_ttl_cache_menu'] = "1 hour";
+$settings['redis_perm_ttl_cache_path'] = "1 hour";
+$settings['redis_perm_ttl_cache_form'] = "20 minutes";
