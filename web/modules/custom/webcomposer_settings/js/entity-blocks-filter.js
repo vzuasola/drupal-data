3b771d7b6a1846faa10bdc12e93d4c177345f3ed
dmabini <darryl.mabini@bayviewtechnology.com>
2019-07-22 15:07:43 +0800

    Reindent js files

--- web/modules/custom/webcomposer_settings/js/entity-blocks-filter.js
+++ web/modules/custom/webcomposer_settings/js/entity-blocks-filter.js
@@ -5,27 +5,27 @@
 
 (function ($, Drupal) {
 
-    'use strict';
-  
-    /**
-     * Webcomposer settings handler.
-     *
-     * @type {Drupal~behavior}
-     */
-    Drupal.behaviors.webcomposerSettingsEntityBlocksFilter = {
-      attach: function (context) {
-        $('input.entity-blocks-filter', context).on('change', function () {
-            var children = 'div.' + $(this).prop('name') + ' input[type="checkbox"]';
-            var isChecked = $(this).prop('checked');
+  'use strict';
 
-            $(children).prop('checked', isChecked);
-        });
+  /**
+   * Webcomposer settings handler.
+   *
+   * @type {Drupal~behavior}
+   */
+  Drupal.behaviors.webcomposerSettingsEntityBlocksFilter = {
+    attach: function (context) {
+      $('input.entity-blocks-filter', context).on('change', function () {
+        var children = 'div.' + $(this).prop('name') + ' input[type="checkbox"]';
+        var isChecked = $(this).prop('checked');
 
-        $('input.entity-blocks-filter-child', context).on('change', function () {
-            $('input.entity-blocks-filter').prop('checked', false);
-        });
-      }
-    };
+        $(children).prop('checked', isChecked);
+      });
+
+      $('input.entity-blocks-filter-child', context).on('change', function () {
+        $('input.entity-blocks-filter').prop('checked', false);
+      });
+    }
+  };
   
   })(jQuery, Drupal);
   
--- web/modules/custom/webcomposer_settings/js/form-fields-filter.js
+++ web/modules/custom/webcomposer_settings/js/form-fields-filter.js
@@ -5,23 +5,23 @@
 
 (function ($, Drupal) {
 
-    'use strict';
-  
-    /**
-     * Webcomposer settings handler.
-     *
-     * @type {Drupal~behavior}
-     */
-    Drupal.behaviors.webcomposerSettingsFormFieldsFilter = {
-      attach: function (context) {
-        $('a.select-all, a.deselect-all', context).click(function (e) {
-            e.preventDefault();
+  'use strict';
 
-            var selector = 'input.' + $(this).data('checkbox-selector') + '-form-fields-filter';
-            $(selector).prop('checked', e.target.className === 'select-all');
-        });
-      }
-    };
+  /**
+   * Webcomposer settings handler.
+   *
+   * @type {Drupal~behavior}
+   */
+  Drupal.behaviors.webcomposerSettingsFormFieldsFilter = {
+    attach: function (context) {
+      $('a.select-all, a.deselect-all', context).click(function (e) {
+        e.preventDefault();
+
+        var selector = 'input.' + $(this).data('checkbox-selector') + '-form-fields-filter';
+        $(selector).prop('checked', e.target.className === 'select-all');
+      });
+    }
+  };
   
   })(jQuery, Drupal);
   
