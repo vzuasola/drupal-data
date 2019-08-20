35df7994861a0392db9b19eb898e9cc144489486
Christian Historillo <christian.historillo@bayviewtechnology.com>
2019-08-20 14:00:30 +0800

    Deposit and Withdrawal (Enhancement)

---
+++ web/sites/jamboree-casino/modules/custom/deposit_and_withdrawal/src/DepositAndWithdrawalEntityTranslationHandler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\deposit_and_withdrawal;
+
+use Drupal\content_translation\ContentTranslationHandler;
+
+/**
+ * Defines the translation handler for deposit_and_withdrawal_entity.
+ */
+class DepositAndWithdrawalEntityTranslationHandler extends ContentTranslationHandler {
+
+  // Override here the needed methods from ContentTranslationHandler.
+
+}
