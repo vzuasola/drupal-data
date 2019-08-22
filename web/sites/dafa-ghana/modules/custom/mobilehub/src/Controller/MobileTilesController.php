ce255bef3ce3f467776b30854bf64b77e4331190
Yunyce de Jesus <Yunyce.DeJesus@bayviewtechnology.com>
2019-08-22 10:53:45 +0800

    [feature-GHANA-5-mobile-tile] resolve sonar issues

--- web/sites/dafa-ghana/modules/custom/mobilehub/src/Controller/MobileTilesController.php
+++ web/sites/dafa-ghana/modules/custom/mobilehub/src/Controller/MobileTilesController.php
@@ -42,7 +42,9 @@ class MobileTilesController extends ControllerBase implements ContainerInjection
    */
   public function revisionPageTitle($mobile_tiles_revision) {
     $mobile_tiles = $this->entityManager()->getStorage('mobile_tiles')->loadRevision($mobile_tiles_revision);
-    return $this->t('Revision of %title from %date', ['%title' => $mobile_tiles->label(), '%date' => format_date($mobile_tiles->getRevisionCreationTime())]);
+    return $this->t('Revision of %title from %date', [
+      '%title' => $mobile_tiles->label(),
+      '%date' => format_date($mobile_tiles->getRevisionCreationTime())]);
   }
 
   /**
@@ -62,11 +64,16 @@ class MobileTilesController extends ControllerBase implements ContainerInjection
     $has_translations = (count($languages) > 1);
     $mobile_tiles_storage = $this->entityManager()->getStorage('mobile_tiles');
 
-    $build['#title'] = $has_translations ? $this->t('@langname revisions for %title', ['@langname' => $langname, '%title' => $mobile_tiles->label()]) : $this->t('Revisions for %title', ['%title' => $mobile_tiles->label()]);
+    $build['#title'] = $has_translations ? $this->t('@langname revisions for %title', [
+      '@langname' => $langname,
+      '%title' => $mobile_tiles->label()
+    ]) : $this->t('Revisions for %title', ['%title' => $mobile_tiles->label()]);
     $header = [$this->t('Revision'), $this->t('Operations')];
 
-    $revert_permission = (($account->hasPermission("revert all mobile tiles revisions") || $account->hasPermission('administer mobile tiles entities')));
-    $delete_permission = (($account->hasPermission("delete all mobile tiles revisions") || $account->hasPermission('administer mobile tiles entities')));
+    $revert_permission = (($account->hasPermission("revert all mobile tiles revisions") ||
+      $account->hasPermission('administer mobile tiles entities')));
+    $delete_permission = (($account->hasPermission("delete all mobile tiles revisions") ||
+      $account->hasPermission('administer mobile tiles entities')));
 
     $rows = [];
 
@@ -88,7 +95,9 @@ class MobileTilesController extends ControllerBase implements ContainerInjection
         // Use revision link to link to revisions that are not active.
         $date = \Drupal::service('date.formatter')->format($revision->getRevisionCreationTime(), 'short');
         if ($vid != $mobile_tiles->getRevisionId()) {
-          $link = $this->l($date, new Url('entity.mobile_tiles.revision', ['mobile_tiles' => $mobile_tiles->id(), 'mobile_tiles_revision' => $vid]));
+          $link = $this->l($date, new Url('entity.mobile_tiles.revision', [
+            'mobile_tiles' => $mobile_tiles->id(),
+            'mobile_tiles_revision' => $vid]));
         }
         else {
           $link = $mobile_tiles->link($date);
@@ -98,7 +107,8 @@ class MobileTilesController extends ControllerBase implements ContainerInjection
         $column = [
           'data' => [
             '#type' => 'inline_template',
-            '#template' => '{% trans %}{{ date }} by {{ username }}{% endtrans %}{% if message %}<p class="revision-log">{{ message }}</p>{% endif %}',
+            '#template' => '{% trans %}{{ date }} by {{ username }}{% endtrans %}
+              {% if message %}<p class="revision-log">{{ message }}</p>{% endif %}',
             '#context' => [
               'date' => $link,
               'username' => \Drupal::service('renderer')->renderPlain($username),
@@ -127,15 +137,22 @@ class MobileTilesController extends ControllerBase implements ContainerInjection
             $links['revert'] = [
               'title' => $this->t('Revert'),
               'url' => $has_translations ?
-              Url::fromRoute('entity.mobile_tiles.translation_revert', ['mobile_tiles' => $mobile_tiles->id(), 'mobile_tiles_revision' => $vid, 'langcode' => $langcode]) :
-              Url::fromRoute('entity.mobile_tiles.revision_revert', ['mobile_tiles' => $mobile_tiles->id(), 'mobile_tiles_revision' => $vid]),
+              Url::fromRoute('entity.mobile_tiles.translation_revert', [
+                'mobile_tiles' => $mobile_tiles->id(),
+                'mobile_tiles_revision' => $vid,
+                'langcode' => $langcode]) :
+              Url::fromRoute('entity.mobile_tiles.revision_revert', [
+                'mobile_tiles' => $mobile_tiles->id(),
+                'mobile_tiles_revision' => $vid]),
             ];
           }
 
           if ($delete_permission) {
             $links['delete'] = [
               'title' => $this->t('Delete'),
-              'url' => Url::fromRoute('entity.mobile_tiles.revision_delete', ['mobile_tiles' => $mobile_tiles->id(), 'mobile_tiles_revision' => $vid]),
+              'url' => Url::fromRoute('entity.mobile_tiles.revision_delete', [
+                'mobile_tiles' => $mobile_tiles->id(),
+                'mobile_tiles_revision' => $vid]),
             ];
           }
 
