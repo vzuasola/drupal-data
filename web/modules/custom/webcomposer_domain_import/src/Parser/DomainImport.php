b25e6b845a9dd556c03a24d57ca34834c8d9465d
Stephanie Aviles <stephanie.aviles@bayviewtechnology.com>
2019-08-27 10:40:18 +0800

    revert fix

--- web/modules/custom/webcomposer_domain_import/src/Parser/DomainImport.php
+++ web/modules/custom/webcomposer_domain_import/src/Parser/DomainImport.php
@@ -530,19 +530,17 @@ class DomainImport {
     $paragraph = [];
     foreach ($variables[$domain]['variables'] as $token => $value) {
         // create paragraph and assign to term
-        if (!is_null($value)) {
-          $paragraph[$token] = Paragraph::create([
-              'type' => 'domain_management_configuration',
-              'field_placeholder_key' => [
-                'value' => $token,
-              ],
-              'field_default_value' => [
-                'value' => $value,
-              ],
-          ]);
-          $paragraph[$token]->save();
-          $termItem['field_add_placeholder'][] = $paragraph[$token];
-        }
+        $paragraph[$token] = Paragraph::create([
+            'type' => 'domain_management_configuration',
+            'field_placeholder_key' => [
+              'value' => $token,
+            ],
+            'field_default_value' => [
+              'value' => $value,
+            ],
+        ]);
+        $paragraph[$token]->save();
+        $termItem['field_add_placeholder'][] = $paragraph[$token];
     }
     $term = Term::create($termItem);
     $term->save();
