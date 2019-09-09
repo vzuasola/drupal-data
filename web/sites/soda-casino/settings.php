2f801ab4c45e70cfd15d76dec4c9243a0dc981b2
Aldrin Nikolai Tomas <AldrinNikolai.Tomas@bayviewtechnology.com>
2019-09-09 13:45:47 +0800

    Adjusted settings.php

--- web/sites/soda-casino/settings.php
+++ web/sites/soda-casino/settings.php
@@ -6,10 +6,14 @@ require $app_root . '/../config/base.settings.php';
 /**
  * The product code
  */
-$settings['product'] = 'product';
+$settings['product'] = 'soda-casino';
 
 /**
  * The main URL prefix for this site instance
  */
-$settings['primary_site_prefix'] = 'poker';
-$settings['install_profile'] = 'config_installer';
+$settings['primary_site_prefix'] = 'soda-casino';
+
+/**
+ * The front end prefix that CKEditor will append for all products
+ */
+$settings['ck_editor_inline_image_prefix'] = '/en/soda-casino';
