f6898440e99d560b12b4d293db5ac5a0dc215f56
ayuson <aubrey.yuson@bayviewtechnology.com>
2019-10-22 17:56:45 +0800

    Settings Updated

--- web/sites/777baby-casino/settings.php
+++ web/sites/777baby-casino/settings.php
@@ -6,5 +6,9 @@ require $app_root . '/../config/base.settings.php';
 /**
  * The product code
  */
-
 $settings['product'] = '777baby-casino';
+
+/**
+ * The front end prefix that CKEditor will append for all products
+ */
+$settings['ck_editor_inline_image_prefix'] = '/';
