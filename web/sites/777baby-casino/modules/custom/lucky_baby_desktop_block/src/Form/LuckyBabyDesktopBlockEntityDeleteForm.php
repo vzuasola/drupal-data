80c778c17106af7f1bf4e4db976224862bb9db8c
chistorillo <Christian.Historillo@bayviewtechnology.com>
2019-10-22 13:20:58 +0800

    Game and non game category blocks

---
+++ web/sites/777baby-casino/modules/custom/lucky_baby_desktop_block/src/Form/LuckyBabyDesktopBlockEntityDeleteForm.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\lucky_baby_desktop_block\Form;
+
+use Drupal\Core\Entity\ContentEntityDeleteForm;
+
+/**
+ * Provides a form for deleting Lucky baby desktop block entity entities.
+ *
+ * @ingroup lucky_baby_desktop_block
+ */
+class LuckyBabyDesktopBlockEntityDeleteForm extends ContentEntityDeleteForm {
+
+
+}
