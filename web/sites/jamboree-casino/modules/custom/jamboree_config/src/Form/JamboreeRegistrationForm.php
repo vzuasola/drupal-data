200f00431158cb347846b539ca5cec2a0606ff71
chistorillo <Christian.Historillo@bayviewtechnology.com>
2019-11-20 15:02:04 +0800

    configurable fields for reg validation

--- web/sites/jamboree-casino/modules/custom/jamboree_config/src/Form/JamboreeRegistrationForm.php
+++ web/sites/jamboree-casino/modules/custom/jamboree_config/src/Form/JamboreeRegistrationForm.php
@@ -201,6 +201,16 @@ class JamboreeRegistrationForm extends FormBase {
       '#title' => $this->t('Registration Error Message if Service is not Available'),
       '#default_value' => $this->get('service_not_available'),
     ];
+    $form['reg_form']['errors']['icore_username_validation'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Registration Error Message if Username is already exists'),
+      '#default_value' => $this->get('icore_username_validation'),
+    ];
+    $form['reg_form']['errors']['icore_email_validation'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Registration Error Message if Email is already exists'),
+      '#default_value' => $this->get('icore_email_validation'),
+    ];
   }
 
   /**
