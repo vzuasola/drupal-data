a2860d403eed26e19b0cb9337293eb7d9c5bc646
Stephanie Aviles <stephanie.aviles@bayviewtechnology.com>
2020-01-17 15:45:59 +0800

    domain form batching

---
+++ web/modules/custom/webcomposer_domain_import/src/Controller/DomainImportController.php
@@ -0,0 +1,72 @@
+<?php
+namespace Drupal\webcomposer_domain_import\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class DomainImportController extends ControllerBase {
+
+  /**
+   * Returns a render-able array for a test page.
+   */
+  public function content($import_file = null) {
+    $build = [
+      '#markup' => 'Domain Import'
+    ];
+    $config = $this->config('webcomposer_config.toggle_configuration');
+    $domain_import = \Drupal::service('webcomposer_domain_import.domain_import');
+    // get how many domains per batch
+    $domain_batch = $config->get('domains_batch');
+    $domain_batch = (int)$domain_batch + 1;
+    $fid = $_GET['import_file'][0];
+
+    // form for masterplaceholders
+    $build['form_masterplaceholder'] = \Drupal::formBuilder()->getForm(
+      '\Drupal\webcomposer_domain_import\Form\BatchImportForm', [
+        'submit_callback' => array('::submitPlaceholder'),
+        'action' => 'Import MASTER PLACEHOLDERS',
+        'fid' => $fid,
+        'id' => 'domain-import-form-placeholder',
+      ]
+    );
+    $domains = $domain_import->getExcelDomains($_GET['import_file'][0]);
+    foreach ($domains as $group => $domain) {
+      // form for domain groups
+      $build["form_domain_group_$group"] = \Drupal::formBuilder()->getForm(
+        '\Drupal\webcomposer_domain_import\Form\BatchImportForm', [
+          'submit_callback' => array('::submitDomainGroup'),
+          'action' => "Import DOMAIN GROUP: $group",
+          'fid' => $fid,
+          'group' => $group,
+          'id' => 'domain-import-form-group',
+        ]
+      );
+      $domain_avg = ceil(count($domain)/$domain_batch);
+      for ($i = 0; $i < $domain_avg; $i++) {
+        // form for domains
+        $domain_slice = array_slice($domain,($i * $domain_batch), $domain_batch);
+        $id = "domain-import-form_". strtolower($group). $i;
+        $build[$id] = \Drupal::formBuilder()->getForm(
+            '\Drupal\webcomposer_domain_import\Form\BatchImportForm', [
+              'submit_callback' => array('::submitDomainBatch'),
+              'action' => "Import DOMAINS: ". implode(", ", $domain_slice),
+              'fid' => $fid,
+              'group' => $group,
+              'id' => $id,
+              'domain_slice' => $domain_slice,
+            ]
+        );
+      }
+    }
+
+    $build['form_removebackup'] = \Drupal::formBuilder()->getForm(
+      '\Drupal\webcomposer_domain_import\Form\BatchImportForm', [
+        'submit_callback' => array('::submitDomainRemoveBackup'),
+        'action' => 'Remove previous import',
+        'fid' => $fid,
+        'id' => 'domain-import-form_remove-backup',
+      ]
+    );
+    return $build;
+  }
+
+}
---
+++ web/modules/custom/webcomposer_domain_import/src/Form/BatchImportForm.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace Drupal\webcomposer_domain_import\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Contribute form.
+ */
+class BatchImportForm extends FormBase {
+
+  /**
+   * DomainImport object.
+   *
+   * @var domainImport
+   */
+  protected $domainImport;
+  protected static $instanceId;
+
+  /**
+   * Constructor.
+   */
+  public function __construct() {
+    $this->domainImport = \Drupal::service('webcomposer_domain_import.domain_import');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    if (empty(self::$instanceId)) {
+      self::$instanceId = 1;
+    }
+    else {
+      self::$instanceId++;
+    }
+  
+    return 'batch_import_form' . self::$instanceId;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, $extras = []) {
+    $form['domains'] = array(
+      '#type' => 'container',
+      '#title' => $extras['group'] . " Batch ",
+    );
+
+    $form['domains']["text"] = array(
+      '#type' => 'html_tag',
+      '#tag' => 'p',
+      '#value' => $extras["action"],
+    );
+    
+    $form['actions']['domains'] = array(
+      '#type' => 'submit',
+      '#value' => $this->t('Import'),
+      '#name' => $extras['id']. "_submit",
+      '#submit' => $extras['submit_callback']
+    );
+   
+    if (isset($extras["domain_slice"])) {
+      $form['domains_list'] = array(
+        '#type' => 'hidden',
+        '#value' => implode(", ", $extras["domain_slice"]),
+      );
+    }
+
+    if (isset($extras["group"])) {
+      $form['group'] = array(
+        '#type' => 'hidden',
+        '#value' => $extras["group"],
+      );
+    }
+
+    $form['#attributes']['id'] = $extras['id'];
+    $form['fid'] = array(
+      '#type' => 'hidden',
+      '#value' => $extras['fid'],
+    );
+
+    return $form;
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+  }
+
+  public function submitPlaceholder(array &$form, FormStateInterface $form_state) {
+    $operations = [
+        [[$this->domainImport, 'importPlaceholder'], [$form_state]],
+    ];
+    $batch = [
+    'title' => t('Importing Domains'),
+    'operations' => $operations,
+    'init_message' => t('Importing placeholders'),
+    ];
+    batch_set($batch);
+  }
+
+  public function submitDomainGroup(array &$form, FormStateInterface $form_state) {
+    $operations = [
+      [[$this->domainImport, 'importDomainGroup'], [$form_state, $form_state->getValue('group')]]
+    ];
+    // domains batch
+    $batch = [
+      'title' => t('Importing Domains'),
+      'operations' => $operations,
+      'init_message' => t('Importing domain group - '. $form_state->getValue('group')),
+    ];
+    batch_set($batch);
+  }
+
+  public function submitDomainBatch(array &$form, FormStateInterface $form_state) {
+    $domain_slice = explode(", ", $form_state->getValue('domains_list'));
+    $operations[] = [[$this->domainImport, 'importDomain'], [$form_state, $domain_slice, $form_state->getValue('group')]];
+    $operations2[] = [[$this->domainImport, 'importDomainTranslated'], [$form_state, $domain_slice]];
+    foreach ($domain_slice as $domain) {
+      $operations3[] = [[$this->domainImport, 'importDomainPlaceholderTrans'], [$form_state, $domain]];
+    }
+    
+    // domains batch in default language
+    $batch = [
+      'title' => t('Importing Domains'),
+      'operations' => $operations,
+      'init_message' => t('Importing domains '),
+    ];
+    batch_set($batch);
+  
+    // translate domains batch
+    $batch = [
+      'title' => t('Translating Domains'),
+      'operations' => $operations2,
+      'init_message' => t('Translating domains'),
+    ];
+    batch_set($batch);
+
+    // translate domains placeholoder overrides
+    $batch = [
+      'title' => t('Translating Paragraphs'),
+      'operations' => $operations3,
+      'init_message' => t('Translating domain placeholders'),
+    ];
+    batch_set($batch);
+  }
+
+  public function submitDomainRemoveBackup(array &$form, FormStateInterface $form_state) {
+    // delete batch
+    $export_time = time();
+    $operations = [
+      [[$this->domainImport, 'deleteParagraphs'], [$form_state, $export_time]],
+      [[$this->domainImport, 'deleteTaxonomies'], [$form_state, $export_time]],
+    ];
+
+    $batch = [
+      'title' => t('Importing Domains'),
+      'operations' => $operations,
+      'init_message' => t('Batch is ending'),
+    ];
+    batch_set($batch);
+  }
+
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // placeholder
+  }
+
+}
