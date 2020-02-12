e1dbaac9abb4b0d4891131c281e61b5f406d3cc2
John Benedict Quitoriano <jquitoriano@bti00011765.ph.esl-asia.com>
2020-02-12 11:47:25 +0800

    updated game filter config

--- web/sites/jamboree-casino/modules/custom/jamboree_config/src/Form/JamboreeGameFilterConfig.php
+++ web/sites/jamboree-casino/modules/custom/jamboree_config/src/Form/JamboreeGameFilterConfig.php
@@ -13,7 +13,7 @@ use Drupal\Core\Datetime\DrupalDateTime;
  *   id = "jamboree_game_filter",
  *   route = {
  *     "title" = "Game Filter Configuration",
- *     "path" = "/admin/config/jamboree/game_filter_configuration",
+ *     "path" = "/admin/config/jamboree/gamefilter_configuration",
  *   },
  *   menu = {
  *     "title" = "Game Filter Configuration",
@@ -29,7 +29,7 @@ class JamboreeGameFilterConfig extends FormBase {
    * {@inheritdoc}
    */
   protected function getEditableConfigNames() {
-    return ['jamboree_config.game_filter'];
+    return ['jamboree_config.gamefilter_config'];
   }
 
   public function form(array $form, FormStateInterface $form_state) {
@@ -54,38 +54,38 @@ class JamboreeGameFilterConfig extends FormBase {
       '#title' => t('Game Filter Configuration'),
       '#group' => 'advanced',
     ];
-    $form['game_filter']['game_filter_title'] = [
+    $form['game_filter']['gamefilter_title'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Game filter title'),
-      '#default_value' => $this->get('game_filter_title'),
+      '#default_value' => $this->get('gamefilter_title'),
       '#translatable' => TRUE,
     ];
-    $form['game_filter']['game_filter_button_label'] = [
+    $form['game_filter']['gamefilter_button_label'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Game filter button label'),
-      '#default_value' => $this->get('game_filter_button_label'),
+      '#default_value' => $this->get('gamefilter_button_label'),
       '#translatable' => TRUE,
     ];
-    $form['game_filter']['game_filter_game_provider'] = [
+    $form['game_filter']['gamefilter_game_provider'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Game provider title'),
-      '#default_value' => $this->get('game_filter_game_provider'),
+      '#default_value' => $this->get('gamefilter_game_provider'),
       '#translatable' => TRUE,
     ];
-    $form['game_filter']['game_filter_apply'] = [
+    $form['game_filter']['gamefilter_apply'] = [
         '#type' => 'textfield',
         '#title' => $this->t('Apply button title'),
-        '#default_value' => $this->get('game_filter_apply'),
+        '#default_value' => $this->get('gamefilter_apply'),
         '#translatable' => TRUE,
     ];
-      $form['game_filter']['game_filter_reset'] = [
+      $form['game_filter']['gamefilter_reset'] = [
         '#type' => 'textfield',
         '#title' => $this->t('Reset button title'),
-        '#default_value' => $this->get('game_filter_reset'),
+        '#default_value' => $this->get('gamefilter_reset'),
         '#translatable' => TRUE,
     ];
-    $d = $this->get('game_filter_description');
-    $form['game_filter']['game_filter_description'] = [
+    $d = $this->get('gamefilter_description');
+    $form['game_filter']['gamefilter_description'] = [
         '#type' => 'text_format',
         '#title' => $this->t('Game filter description'),
         '#default_value' => $d['value'],
