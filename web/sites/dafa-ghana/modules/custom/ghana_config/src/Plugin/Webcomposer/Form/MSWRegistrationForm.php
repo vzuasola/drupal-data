f3eb022829f2cd7023f73e404979f277c3a4ac44
Jerome Michael <jerome.michael@bayviewtechnology.com>
2020-03-19 14:33:31 +0800

    Created MSW Registration form temporarily in dafa-ghana

--- web/sites/dafa-ghana/modules/custom/ghana_config/src/Plugin/Webcomposer/Form/MSWRegistrationForm.php
+++ web/sites/dafa-ghana/modules/custom/ghana_config/src/Plugin/Webcomposer/Form/MSWRegistrationForm.php
@@ -77,6 +77,28 @@ use Drupal\webcomposer_form_manager\WebcomposerFormInterface;
           ],
         ],
 
+        'email' => [
+          'name' => 'Email',
+          'type' => 'textfield',
+          'settings' => [
+            'label' => [
+              '#title' => 'Email Label',
+              '#type' => 'textfield',
+              '#description' => 'The Label for Email field',
+            ],
+            'placeholder' => [
+              '#title' => 'Email placeholder label',
+              '#type' => 'textfield',
+              '#description' => 'label for Email field placeholder',
+            ],
+            'annotation' => [
+              '#title' => 'Email Annotation text',
+              '#type' => 'textarea',
+              '#description' => 'field annotation that will be displayed on focus',
+            ],
+          ],
+        ],
+
         'password' => [
           'name' => 'Password',
           'type' => 'password',
@@ -138,128 +160,117 @@ use Drupal\webcomposer_form_manager\WebcomposerFormInterface;
           ],
         ],
 
-        'email' => [
-          'name' => 'Email',
+        'rfidpin' => [
+          'name' => 'RFID Pin',
           'type' => 'textfield',
           'settings' => [
             'label' => [
-              '#title' => 'Email Label',
+              '#title' => 'RFID Pin label',
               '#type' => 'textfield',
-              '#description' => 'The Label for Email field',
+              '#description' => 'label for RFID Pin field',
+              '#default_value' => 'RFID Pin',
             ],
             'placeholder' => [
-              '#title' => 'Email placeholder label',
+              '#title' => 'RFID Pin placeholder label',
               '#type' => 'textfield',
-              '#description' => 'label for Email field placeholder',
+              '#description' => 'label for RFID Pin field placeholder',
+              '#default_value' => 'RFID Pin',
             ],
             'annotation' => [
-              '#title' => 'Email Annotation text',
+              '#title' => 'RFID Pin Annotation text',
               '#type' => 'textarea',
               '#description' => 'field annotation that will be displayed on focus',
             ],
           ],
         ],
 
-        'breakline_1' => [
-          'name' => 'Break Line Markup',
+        // 'first_name' => [
+        //   'name' => 'First Name',
+        //   'type' => 'textfield',
+        //   'settings' => [
+        //     'label' => [
+        //       '#title' => 'First Name Label',
+        //       '#type' => 'textfield',
+        //       '#description' => 'The label for the First Name field',
+        //       '#default_value' => 'First Name',
+        //     ],
+        //     'placeholder' => [
+        //       '#title' => 'First name placeholder label',
+        //       '#type' => 'textfield',
+        //       '#description' => 'label for First name field placeholder',
+        //       '#default_value' => 'First Name',
+        //     ],
+        //     'annotation' => [
+        //       '#title' => 'First Name Annotation text',
+        //       '#type' => 'textarea',
+        //       '#description' => 'field annotation that will be displayed on focus',
+        //     ],
+        //   ],
+        // ],
+
+        // 'last_name' => [
+        //   'name' => 'Last Name',
+        //   'type' => 'textfield',
+        //   'settings' => [
+        //     'label' => [
+        //       '#title' => 'Last Name Label',
+        //       '#type' => 'textfield',
+        //       '#description' => 'The label for the Last Name field',
+        //       '#default_value' => 'Last Name',
+        //     ],
+        //     'placeholder' => [
+        //       '#title' => 'Last name placeholder label',
+        //       '#type' => 'textfield',
+        //       '#description' => 'label for Last name field placeholder',
+        //       '#default_value' => 'Last Name',
+        //     ],
+        //     'annotation' => [
+        //       '#title' => 'Last Name Annotation text',
+        //       '#type' => 'textarea',
+        //       '#description' => 'field annotation that will be displayed on focus',
+        //     ],
+        //   ],
+        // ],
+
+        // 'birthdate' => [
+        //   'name' => 'Date of birth',
+        //   'type' => 'textfield',
+        //   'settings' => [
+        //     'label' => [
+        //       '#title' => 'Date of Birth Label',
+        //       '#type' => 'textfield',
+        //       '#description' => 'The Label for the Birthdate field',
+        //       '#default_value' => 'Date of Birth',
+        //     ],
+        //     'placeholder' => [
+        //       '#title' => 'Birthdate placeholder label',
+        //       '#type' => 'textfield',
+        //       '#description' => 'label for Birthdate field placeholder',
+        //       '#default_value' => 'DD/MM/YYYY',
+        //     ],
+        //     'date-format' => [
+        //       '#title' => 'Birthdate format',
+        //       '#type' => 'textfield',
+        //       '#description' => 'Date Format for Birthdate',
+        //       '#default_value' => 'DD/MM/YYYY',
+        //     ],
+        //     'annotation' => [
+        //       '#title' => 'Birthdate Annotation text',
+        //       '#type' => 'textarea',
+        //       '#description' => 'field annotation that will be displayed on focus',
+        //     ],
+        //   ],
+        // ],
+
+        'security_questions_title' => [
+          'name' => 'Security Questions Title Markup',
           'type' => 'markup',
           'settings' => [
             'markup' => [
-              '#title' => 'Horizontal Line Markup',
+              '#title' => 'Security Questions Title Markup',
               '#type' => 'textarea',
-              '#description' => 'A Markup text breakline for registration form',
-              '#default_value' => '<hr>',
-            ],
-          ],
-        ],
-
-        'breakline_2' => [
-          'name' => 'Break Line Markup',
-          'type' => 'markup',
-          'settings' => [
-            'markup' => [
-              '#title' => 'Horizontal Line Markup',
-              '#type' => 'textarea',
-              '#description' => 'A Markup text breakline for registration form',
-              '#default_value' => '<hr>',
-            ],
-          ],
-        ],
-
-        'first_name' => [
-          'name' => 'First Name',
-          'type' => 'textfield',
-          'settings' => [
-            'label' => [
-              '#title' => 'First Name Label',
-              '#type' => 'textfield',
-              '#description' => 'The label for the First Name field',
-              '#default_value' => 'First Name',
-            ],
-            'placeholder' => [
-              '#title' => 'First name placeholder label',
-              '#type' => 'textfield',
-              '#description' => 'label for First name field placeholder',
-              '#default_value' => 'First Name',
-            ],
-            'annotation' => [
-              '#title' => 'First Name Annotation text',
-              '#type' => 'textarea',
-              '#description' => 'field annotation that will be displayed on focus',
-            ],
-          ],
-        ],
-
-        'last_name' => [
-          'name' => 'Last Name',
-          'type' => 'textfield',
-          'settings' => [
-            'label' => [
-              '#title' => 'Last Name Label',
-              '#type' => 'textfield',
-              '#description' => 'The label for the Last Name field',
-              '#default_value' => 'Last Name',
-            ],
-            'placeholder' => [
-              '#title' => 'Last name placeholder label',
-              '#type' => 'textfield',
-              '#description' => 'label for Last name field placeholder',
-              '#default_value' => 'Last Name',
-            ],
-            'annotation' => [
-              '#title' => 'Last Name Annotation text',
-              '#type' => 'textarea',
-              '#description' => 'field annotation that will be displayed on focus',
-            ],
-          ],
-        ],
-
-        'birthdate' => [
-          'name' => 'Date of birth',
-          'type' => 'textfield',
-          'settings' => [
-            'label' => [
-              '#title' => 'Date of Birth Label',
-              '#type' => 'textfield',
-              '#description' => 'The Label for the Birthdate field',
-              '#default_value' => 'Date of Birth',
-            ],
-            'placeholder' => [
-              '#title' => 'Birthdate placeholder label',
-              '#type' => 'textfield',
-              '#description' => 'label for Birthdate field placeholder',
-              '#default_value' => 'DD/MM/YYYY',
-            ],
-            'date-format' => [
-              '#title' => 'Birthdate format',
-              '#type' => 'textfield',
-              '#description' => 'Date Format for Birthdate',
-              '#default_value' => 'DD/MM/YYYY',
-            ],
-            'annotation' => [
-              '#title' => 'Birthdate Annotation text',
-              '#type' => 'textarea',
-              '#description' => 'field annotation that will be displayed on focus',
+              '#description' => 'A Markup text for the Security questions title.',
+              '#default_value' => 'Security Questions<hr>',
             ],
           ],
         ],
@@ -271,7 +282,7 @@ use Drupal\webcomposer_form_manager\WebcomposerFormInterface;
             'label' => [
               '#title' => 'Security Question 1 Label',
               '#type' => 'textfield',
-              '#description' => 'The dLabel for the Currency field',
+              '#description' => 'The Label for the Currency field',
               '#default_value' => 'Security Question 1',
             ],
             'placeholder' => [
@@ -290,11 +301,7 @@ use Drupal\webcomposer_form_manager\WebcomposerFormInterface;
               '#title' => 'Security Question 1 Choices',
               '#type' => 'textarea',
               '#description' => 'Provide a pipe separated key value pair. <br> <small>Example key|My Value</small>',
-              '#default_value' => implode(PHP_EOL, [
-                '1|What is your mother’s maiden name?',
-                '2|What is your father’s first name?',
-                '3|What is the model of your first car?',
-              ]),
+              '#default_value' => $questions,
             ],
           ],
         ],
@@ -349,11 +356,7 @@ use Drupal\webcomposer_form_manager\WebcomposerFormInterface;
               '#title' => 'Security Question 2 Choices',
               '#type' => 'textarea',
               '#description' => 'Provide a pipe separated key value pair. <br> <small>Example key|My Value</small>',
-              '#default_value' => implode(PHP_EOL, [
-                '1|What is your mother’s maiden name?',
-                '2|What is your father’s first name?',
-                '3|What is the model of your first car?',
-              ]),
+              '#default_value' => $questions,
             ],
           ],
         ],
@@ -382,71 +385,58 @@ use Drupal\webcomposer_form_manager\WebcomposerFormInterface;
           ],
         ],
 
-        'breakline_3' => [
-          'name' => 'Break Line Markup',
-          'type' => 'markup',
-          'settings' => [
-            'markup' => [
-              '#title' => 'Horizontal Line Markup',
-              '#type' => 'textarea',
-              '#description' => 'A Markup text breakline for registration form',
-              '#default_value' => '<hr>',
-            ],
-          ],
-        ],
+        // 'bonus_code' => [
+        //   'name' => 'Bonus Code',
+        //   'type' => 'textfield',
+        //   'settings' => [
+        //     'label' => [
+        //       '#title' => 'Bonus Code label',
+        //       '#type' => 'textfield',
+        //       '#description' => 'label for Bonus Code field',
+        //     ],
+        //     'placeholder' => [
+        //       '#title' => 'Bonus code placeholder label',
+        //       '#type' => 'textfield',
+        //       '#description' => 'label for Bonus code field placeholder',
+        //     ],
+        //     'annotation' => [
+        //       '#title' => 'Bonus code Annotation text',
+        //       '#type' => 'textarea',
+        //       '#description' => 'field annotation that will be displayed on focus',
+        //     ],
+        //     'is_hidden' => [
+        //       '#title' => 'Hide Bonus Code',
+        //       '#type' => 'checkbox',
+        //       '#description' => 'Enable or disable bonus code',
+        //     ],
+        //   ],
+        // ],
 
-        'bonus_code' => [
-          'name' => 'Bonus Code',
-          'type' => 'textfield',
-          'settings' => [
-            'label' => [
-              '#title' => 'Bonus Code label',
-              '#type' => 'textfield',
-              '#description' => 'label for Bonus Code field',
-            ],
-            'placeholder' => [
-              '#title' => 'Bonus code placeholder label',
-              '#type' => 'textfield',
-              '#description' => 'label for Bonus code field placeholder',
-            ],
-            'annotation' => [
-              '#title' => 'Bonus code Annotation text',
-              '#type' => 'textarea',
-              '#description' => 'field annotation that will be displayed on focus',
-            ],
-            'is_hidden' => [
-              '#title' => 'Hide Bonus Code',
-              '#type' => 'checkbox',
-              '#description' => 'Enable or disable bonus code',
-            ],
-          ],
-        ],
+        // 'accept_terms' => [
+        //   'name' => 'Accept Terms Markup',
+        //   'type' => 'checkbox',
+        //   'settings' => [
+        //     'markup' => [
+        //       '#title' => 'Accept Terms Text',
+        //       '#type' => 'textarea',
+        //       '#description' => 'A Markup text for accept terms and conditions beside the submit button',
+        //       '#default_value' => 'I have read and accept the Terms and Condition',
+        //     ],
+        //   ],
+        // ],
 
-        'accept_terms' => [
-          'name' => 'Accept Terms Markup',
-          'type' => 'checkbox',
-          'settings' => [
-            'markup' => [
-              '#title' => 'Accept Terms Text',
-              '#type' => 'textarea',
-              '#description' => 'A Markup text for accept terms and conditions beside the submit button',
-              '#default_value' => 'I have read and accept the Terms and Condition',
-            ],
-          ],
-        ],
-
-        'legal_age' => [
-          'name' => 'Legal Age Markup',
-          'type' => 'checkbox',
-          'settings' => [
-            'markup' => [
-              '#title' => 'Legal Age Text',
-              '#type' => 'textarea',
-              '#description' => 'A Markup text for accept terms and conditions beside the submit button',
-              '#default_value' => 'I am at least 18 years old',
-            ],
-          ],
-        ],
+        // 'legal_age' => [
+        //   'name' => 'Legal Age Markup',
+        //   'type' => 'checkbox',
+        //   'settings' => [
+        //     'markup' => [
+        //       '#title' => 'Legal Age Text',
+        //       '#type' => 'textarea',
+        //       '#description' => 'A Markup text for accept terms and conditions beside the submit button',
+        //       '#default_value' => 'I am at least 18 years old',
+        //     ],
+        //   ],
+        // ],
 
         'submit' => [
           'name' => 'Submit',
