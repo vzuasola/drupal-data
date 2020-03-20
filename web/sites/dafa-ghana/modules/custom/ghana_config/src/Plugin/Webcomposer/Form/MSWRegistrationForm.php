b12be1b8ef794bd2953646ca918dae0fee197d4c
Jerome Michael <jerome.michael@bayviewtechnology.com>
2020-03-20 10:44:19 +0800

    Applied all the fields needed for MSW Registration

--- web/sites/dafa-ghana/modules/custom/ghana_config/src/Plugin/Webcomposer/Form/MSWRegistrationForm.php
+++ web/sites/dafa-ghana/modules/custom/ghana_config/src/Plugin/Webcomposer/Form/MSWRegistrationForm.php
@@ -53,6 +53,367 @@ use Drupal\webcomposer_form_manager\WebcomposerFormInterface;
           ],
         ],
 
+        'column_1_open' => [
+          'name' => 'First column markup open',
+          'type' => 'markup',
+          'settings' => [
+            'markup' => [
+              '#title' => 'First column markup open',
+              '#type' => 'textarea',
+              '#description' => 'Opening the first column of registration form',
+              '#default_value' => '<div>',
+            ],
+          ],
+        ],
+
+        'last_name' => [
+          'name' => 'Last Name',
+          'type' => 'textfield',
+          'settings' => [
+            'label' => [
+              '#title' => 'Last Name Label',
+              '#type' => 'textfield',
+              '#description' => 'The label for the Last Name field',
+              '#default_value' => 'Last Name',
+            ],
+            'placeholder' => [
+              '#title' => 'Last name placeholder label',
+              '#type' => 'textfield',
+              '#description' => 'label for Last name field placeholder',
+              '#default_value' => 'Last Name',
+            ],
+            'annotation' => [
+              '#title' => 'Last Name Annotation text',
+              '#type' => 'textarea',
+              '#description' => 'field annotation that will be displayed on focus',
+            ],
+          ],
+        ],
+
+        'first_name' => [
+          'name' => 'First Name',
+          'type' => 'textfield',
+          'settings' => [
+            'label' => [
+              '#title' => 'First Name Label',
+              '#type' => 'textfield',
+              '#description' => 'The label for the First Name field',
+              '#default_value' => 'First Name',
+            ],
+            'placeholder' => [
+              '#title' => 'First name placeholder label',
+              '#type' => 'textfield',
+              '#description' => 'label for First name field placeholder',
+              '#default_value' => 'First Name',
+            ],
+            'annotation' => [
+              '#title' => 'First Name Annotation text',
+              '#type' => 'textarea',
+              '#description' => 'field annotation that will be displayed on focus',
+            ],
+          ],
+        ],
+
+        'middle_name' => [
+          'name' => 'Middle Name',
+          'type' => 'textfield',
+          'settings' => [
+            'label' => [
+              '#title' => 'Middle Name Label',
+              '#type' => 'textfield',
+              '#description' => 'The label for the Middle Name field',
+              '#default_value' => 'Middle Name',
+            ],
+            'placeholder' => [
+              '#title' => 'Middle name placeholder label',
+              '#type' => 'textfield',
+              '#description' => 'label for Middle name field placeholder',
+              '#default_value' => 'Middle Name',
+            ],
+            'annotation' => [
+              '#title' => 'Middle Name Annotation text',
+              '#type' => 'textarea',
+              '#description' => 'field annotation that will be displayed on focus',
+            ],
+          ],
+        ],
+
+        'birthdate' => [
+          'name' => 'Date of birth',
+          'type' => 'textfield',
+          'settings' => [
+            'label' => [
+              '#title' => 'Date of Birth Label',
+              '#type' => 'textfield',
+              '#description' => 'The Label for the Birthdate field',
+              '#default_value' => 'Date of Birth',
+            ],
+            'placeholder' => [
+              '#title' => 'Birthdate placeholder label',
+              '#type' => 'textfield',
+              '#description' => 'label for Birthdate field placeholder',
+              '#default_value' => 'DD/MM/YYYY',
+            ],
+            'date-format' => [
+              '#title' => 'Birthdate format',
+              '#type' => 'textfield',
+              '#description' => 'Date Format for Birthdate',
+              '#default_value' => 'DD/MM/YYYY',
+            ],
+            'annotation' => [
+              '#title' => 'Birthdate Annotation text',
+              '#type' => 'textarea',
+              '#description' => 'field annotation that will be displayed on focus',
+            ],
+          ],
+        ],
+
+        'province' => [
+          'name' => 'Province',
+          'type' => 'select',
+          'settings' => [
+            'label' => [
+              '#title' => 'Province Label',
+              '#type' => 'textfield',
+              '#description' => 'The Label for the province field',
+              '#default_value' => 'Province',
+            ],
+            'placeholder' => [
+              '#title' => 'Choose a Province',
+              '#type' => 'textfield',
+              '#description' => 'Placeholder value for this textfield',
+              '#default_value' => 'Province',
+            ],
+            'groups' => [
+              '#title' => 'Group Name',
+              '#type' => 'textfield',
+              '#description' => 'Group name for Province',
+              '#default_value' => 'province',
+            ],
+            'choices' => [
+              '#title' => 'Province Choices',
+              '#type' => 'textarea',
+              '#description' => 'Provide a pipe separated key value pair. <br> <small>Example key|My Value</small>',
+              '#default_value' => $province,
+            ],
+          ],
+        ],
+
+        'city' => [
+          'name' => 'City',
+          'type' => 'select',
+          'settings' => [
+            'label' => [
+              '#title' => 'City Label',
+              '#type' => 'textfield',
+              '#description' => 'The Label for the Currency field',
+              '#default_value' => 'City',
+            ],
+            'placeholder' => [
+              '#title' => 'Choose a City',
+              '#type' => 'textfield',
+              '#description' => 'Placeholder value for this textfield',
+              '#default_value' => 'City',
+            ],
+            'groups' => [
+              '#title' => 'Group Name',
+              '#type' => 'textfield',
+              '#description' => 'Group name for Security Answer 1',
+              '#default_value' => 'city',
+            ],
+            'choices' => [
+              '#title' => 'City Choices',
+              '#type' => 'textarea',
+              '#description' => 'Provide a pipe separated key value pair. <br> <small>Example key|My Value</small>',
+              '#default_value' => $city,
+            ],
+          ],
+        ],
+
+        'address' => [
+          'name' => 'Address',
+          'type' => 'textfield',
+          'settings' => [
+            'label' => [
+              '#title' => 'Address Label',
+              '#type' => 'textfield',
+              '#description' => 'The label for the Address field',
+              '#default_value' => 'Address',
+            ],
+            'placeholder' => [
+              '#title' => 'Address placeholder label',
+              '#type' => 'textfield',
+              '#description' => 'label for Address field placeholder',
+              '#default_value' => 'Address',
+            ],
+            'annotation' => [
+              '#title' => 'Address Annotation text',
+              '#type' => 'textarea',
+              '#description' => 'field annotation that will be displayed on focus',
+            ],
+          ],
+        ],
+
+        'mobile_phone' => [
+          'name' => 'Mobile phone',
+          'type' => 'textfield',
+          'settings' => [
+            'label' => [
+              '#title' => 'Mobile phone Label',
+              '#type' => 'textfield',
+              '#description' => 'The label for the Mobile phone field',
+              '#default_value' => 'Mobile phone',
+            ],
+            'placeholder' => [
+              '#title' => 'Mobile phone placeholder label',
+              '#type' => 'textfield',
+              '#description' => 'label for Mobile phone field placeholder',
+              '#default_value' => 'Mobile phone',
+            ],
+            'annotation' => [
+              '#title' => 'Mobile phone Annotation text',
+              '#type' => 'textarea',
+              '#description' => 'field annotation that will be displayed on focus',
+            ],
+          ],
+        ],
+
+        'id1' => [
+          'name' => 'Select ID 1',
+          'type' => 'select',
+          'settings' => [
+            'label' => [
+              '#title' => 'Select ID 1 Label',
+              '#type' => 'textfield',
+              '#description' => 'The Label for the Currency field',
+              '#default_value' => 'Select ID 1',
+            ],
+            'placeholder' => [
+              '#title' => 'Select ID 1',
+              '#type' => 'textfield',
+              '#description' => 'Placeholder value for this textfield',
+              '#default_value' => 'Select ID 1',
+            ],
+            'groups' => [
+              '#title' => 'Group Name',
+              '#type' => 'textfield',
+              '#description' => 'Group name for Security Answer 1',
+              '#default_value' => 'Select ID 1',
+            ],
+            'choices' => [
+              '#title' => 'Select ID 1 Choices',
+              '#type' => 'textarea',
+              '#description' => 'Provide a pipe separated key value pair. <br> <small>Example key|My Value</small>',
+              '#default_value' => $id1,
+            ],
+          ],
+        ],
+
+        'id2' => [
+          'name' => 'Select ID 2',
+          'type' => 'select',
+          'settings' => [
+            'label' => [
+              '#title' => 'Select ID 2 Label',
+              '#type' => 'textfield',
+              '#description' => 'The Label for the Currency field',
+              '#default_value' => 'Select ID 2',
+            ],
+            'placeholder' => [
+              '#title' => 'Select ID 2',
+              '#type' => 'textfield',
+              '#description' => 'Placeholder value for this textfield',
+              '#default_value' => 'Select ID 2',
+            ],
+            'groups' => [
+              '#title' => 'Group Name',
+              '#type' => 'textfield',
+              '#description' => 'Group name for Security Answer 1',
+              '#default_value' => 'Select ID 2',
+            ],
+            'choices' => [
+              '#title' => 'Select ID 2 Choices',
+              '#type' => 'textarea',
+              '#description' => 'Provide a pipe separated key value pair. <br> <small>Example key|My Value</small>',
+              '#default_value' => $id2,
+            ],
+          ],
+        ],
+
+        'id_number1' => [
+          'name' => 'ID Number 1',
+          'type' => 'textfield',
+          'settings' => [
+            'label' => [
+              '#title' => 'ID Number 1 Label',
+              '#type' => 'textfield',
+              '#description' => 'The label for the ID Number 1 field',
+              '#default_value' => 'ID Number 1',
+            ],
+            'placeholder' => [
+              '#title' => 'ID Number 1 placeholder label',
+              '#type' => 'textfield',
+              '#description' => 'label for ID Number 1 field placeholder',
+              '#default_value' => 'ID Number 1',
+            ],
+            'annotation' => [
+              '#title' => 'ID Number 1 Annotation text',
+              '#type' => 'textarea',
+              '#description' => 'field annotation that will be displayed on focus',
+            ],
+          ],
+        ],
+
+        'id_number2' => [
+          'name' => 'ID Number 2',
+          'type' => 'textfield',
+          'settings' => [
+            'label' => [
+              '#title' => 'ID Number 2 Label',
+              '#type' => 'textfield',
+              '#description' => 'The label for the ID Number 2 field',
+              '#default_value' => 'ID Number 2',
+            ],
+            'placeholder' => [
+              '#title' => 'ID Number 2 placeholder label',
+              '#type' => 'textfield',
+              '#description' => 'label for ID Number 2 field placeholder',
+              '#default_value' => 'ID Number 2',
+            ],
+            'annotation' => [
+              '#title' => 'ID Number 2 Annotation text',
+              '#type' => 'textarea',
+              '#description' => 'field annotation that will be displayed on focus',
+            ],
+          ],
+        ],
+
+        'column_1_close' => [
+          'name' => 'First column markup closing',
+          'type' => 'markup',
+          'settings' => [
+            'markup' => [
+              '#title' => 'First column markup closing',
+              '#type' => 'textarea',
+              '#description' => 'closing the first column of registration form',
+              '#default_value' => '</div>',
+            ],
+          ],
+        ],
+
+        'column_2_open' => [
+          'name' => 'Second column markup open',
+          'type' => 'markup',
+          'settings' => [
+            'markup' => [
+              '#title' => 'Second column markup open',
+              '#type' => 'textarea',
+              '#description' => 'Opening the Second column of registration form',
+              '#default_value' => '<div>',
+            ],
+          ],
+        ],
+
         'username' => [
           'name' => 'Username',
           'type' => 'textfield',
@@ -183,84 +544,6 @@ use Drupal\webcomposer_form_manager\WebcomposerFormInterface;
             ],
           ],
         ],
-
-        // 'first_name' => [
-        //   'name' => 'First Name',
-        //   'type' => 'textfield',
-        //   'settings' => [
-        //     'label' => [
-        //       '#title' => 'First Name Label',
-        //       '#type' => 'textfield',
-        //       '#description' => 'The label for the First Name field',
-        //       '#default_value' => 'First Name',
-        //     ],
-        //     'placeholder' => [
-        //       '#title' => 'First name placeholder label',
-        //       '#type' => 'textfield',
-        //       '#description' => 'label for First name field placeholder',
-        //       '#default_value' => 'First Name',
-        //     ],
-        //     'annotation' => [
-        //       '#title' => 'First Name Annotation text',
-        //       '#type' => 'textarea',
-        //       '#description' => 'field annotation that will be displayed on focus',
-        //     ],
-        //   ],
-        // ],
-
-        // 'last_name' => [
-        //   'name' => 'Last Name',
-        //   'type' => 'textfield',
-        //   'settings' => [
-        //     'label' => [
-        //       '#title' => 'Last Name Label',
-        //       '#type' => 'textfield',
-        //       '#description' => 'The label for the Last Name field',
-        //       '#default_value' => 'Last Name',
-        //     ],
-        //     'placeholder' => [
-        //       '#title' => 'Last name placeholder label',
-        //       '#type' => 'textfield',
-        //       '#description' => 'label for Last name field placeholder',
-        //       '#default_value' => 'Last Name',
-        //     ],
-        //     'annotation' => [
-        //       '#title' => 'Last Name Annotation text',
-        //       '#type' => 'textarea',
-        //       '#description' => 'field annotation that will be displayed on focus',
-        //     ],
-        //   ],
-        // ],
-
-        // 'birthdate' => [
-        //   'name' => 'Date of birth',
-        //   'type' => 'textfield',
-        //   'settings' => [
-        //     'label' => [
-        //       '#title' => 'Date of Birth Label',
-        //       '#type' => 'textfield',
-        //       '#description' => 'The Label for the Birthdate field',
-        //       '#default_value' => 'Date of Birth',
-        //     ],
-        //     'placeholder' => [
-        //       '#title' => 'Birthdate placeholder label',
-        //       '#type' => 'textfield',
-        //       '#description' => 'label for Birthdate field placeholder',
-        //       '#default_value' => 'DD/MM/YYYY',
-        //     ],
-        //     'date-format' => [
-        //       '#title' => 'Birthdate format',
-        //       '#type' => 'textfield',
-        //       '#description' => 'Date Format for Birthdate',
-        //       '#default_value' => 'DD/MM/YYYY',
-        //     ],
-        //     'annotation' => [
-        //       '#title' => 'Birthdate Annotation text',
-        //       '#type' => 'textarea',
-        //       '#description' => 'field annotation that will be displayed on focus',
-        //     ],
-        //   ],
-        // ],
 
         'security_questions_title' => [
           'name' => 'Security Questions Title Markup',
@@ -447,6 +730,19 @@ use Drupal\webcomposer_form_manager\WebcomposerFormInterface;
               '#type' => 'textfield',
               '#description' => 'Label for the submit button',
               '#default_value' => 'Submit',
+            ],
+          ],
+        ],
+
+        'column_2_close' => [
+          'name' => 'Second column markup closing',
+          'type' => 'markup',
+          'settings' => [
+            'markup' => [
+              '#title' => 'Second column markup closing',
+              '#type' => 'textarea',
+              '#description' => 'closing the second column of registration form',
+              '#default_value' => '</div>',
             ],
           ],
         ],
