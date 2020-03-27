aa1983eeb7b8a51a028a32bdb742db40d7e70763
John Patric Rivera <johnpatric.rivera@bayviewtechnology.com>
2020-03-27 14:52:06 +0800

    Create the initial handler for the submit form

--- web/modules/custom/webcomposer_domains_configuration_v2/src/Form/ImportForm.php
+++ web/modules/custom/webcomposer_domains_configuration_v2/src/Form/ImportForm.php
@@ -23,16 +23,18 @@ use Drupal\webcomposer_config_schema\Annotation\WebcomposerConfigPlugin;
  *   },
  * )
  */
-
-class ImportForm extends FormBase {
+class ImportForm extends FormBase
+{
   const FORM_ID = 'import_domains_form';
   protected static $instanceId;
+  protected $enabled;
 
   /**
    * Constructor.
    */
-  public function __construct() {
-
+  public function __construct()
+  {
+    $this->enabled = true;
   }
 
   /**
@@ -40,24 +42,27 @@ class ImportForm extends FormBase {
    *
    * @return string
    */
-  public function getFormId() {
+  public function getFormId()
+  {
     return self::FORM_ID;
   }
 
   /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state) {
+  public function buildForm(array $form, FormStateInterface $form_state)
+  {
     $form['import_file'] = [
       '#type' => 'managed_file',
       '#title' => $this->t('Import file'),
       '#required' => TRUE,
-      '#upload_validators'  => [
+      '#upload_validators' => [
         'file_validate_extensions' => ['csv xml xlsx'],
         'file_validate_size' => [25600000],
       ],
       '#upload_location' => 'public://taxonomy_files/',
       '#description' => t('Upload a file to Import Domains! Supported format xlsx'),
+      '#disabled' => !$this->enabled
     ];
 
     $form['actions'] = [
@@ -67,13 +72,37 @@ class ImportForm extends FormBase {
     $form['actions']['next'] = [
       '#type' => 'submit',
       '#value' => $this->t('Submit'),
+      '#disabled' => !$this->enabled
     ];
     return $form;
   }
-  public function validateForm(array &$form, FormStateInterface $form_state) {
-  }
-  public function submitForm(array &$form, FormStateInterface $form_state) {
 
+  public function validateForm(array &$form, FormStateInterface $form_state)
+  {
+    parent::validateForm($form, $form_state);
+  }
+
+  public function submitForm(array &$form, FormStateInterface $form_state)
+  {
+    // If this module is not enabled do nothing
+    if (!$this->enabled) {
+      drupal_set_message('Failed to import domains due to the following reason(s): Module not enabled!', 'error');
+    }
+
+    // TODO CREATE VALIDATIONS HERE
+    if (false) {
+      $error = ['foo', 'bar', 'baz'];
+      drupal_set_message('Failed to import domains due to the following reason(s): ' . implode('<br>', $error)
+        , 'error');
+    } else {
+      // TODO: Create the excel parser here
+      // TODO: Create the redis client here
+      // TODO: Populate the redis here
+      // TODO: optional create a progress status while importing
+      drupal_set_message('The domains was imported successfully.');
+    }
+
+    // $form_state->setResponse($this-> buildResponse());
   }
 
 }
