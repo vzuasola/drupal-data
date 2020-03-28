2fd80024c48a4fb9876506999e0103bffb982f82
John Patric Rivera <johnpatric.rivera@bayviewtechnology.com>
2020-03-28 22:04:02 +0800

    Create Redis Service & Storage Interface

---
+++ web/modules/custom/webcomposer_domains_configuration_v2/src/Storage/RedisService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\webcomposer_domains_configuration_v2\Storage;
+
+/**
+ * Class RedisService.
+ */
+class RedisService implements StorageInterface {
+
+  /**
+   * Constructs a new RedisService object.
+   */
+  public function __construct() {
+
+  }
+
+}
---
+++ web/modules/custom/webcomposer_domains_configuration_v2/src/Storage/StorageInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Drupal\webcomposer_domains_configuration_v2\Storage;
+
+/**
+ * Interface StorageInterface.
+ */
+interface StorageInterface {
+
+
+}
