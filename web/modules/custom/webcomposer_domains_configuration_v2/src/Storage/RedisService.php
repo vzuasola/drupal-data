da24094a090be229acc6c7d65dff40503bf012bf
John Patric Rivera <johnpatric.rivera@bayviewtechnology.com>
2020-03-30 15:34:52 +0800

    Updated the redis service

--- web/modules/custom/webcomposer_domains_configuration_v2/src/Storage/RedisService.php
+++ web/modules/custom/webcomposer_domains_configuration_v2/src/Storage/RedisService.php
@@ -2,24 +2,57 @@
 
 namespace Drupal\webcomposer_domains_configuration_v2\Storage;
 
+use Predis\Client as Redis;
+
 /**
  * Class RedisService.
  */
 class RedisService implements StorageInterface {
 
   /**
+   *
+   */
+  protected const DOMAIN_NAMESPACE = "domains:";
+  // TODO: Create a config for this
+  /**
+   * @var array
+   */
+  protected $redisConfig = [
+    'client' => 'tcp://10.5.0.7:6379',
+    'options' => [
+      'parameters' => [
+        'database' => 2
+      ]
+    ]
+  ];
+
+  /**
+   * @var Redis
+   */
+  private $redis;
+
+  /**
    * Constructs a new RedisService object.
    */
   public function __construct() {
-
+    $this->redis = new Redis($this->redisConfig['client'], $this->redisConfig['options']);
   }
 
+  /**
+   * @param string $key
+   * @param array $data
+   * @return bool|void
+   */
   public function set(string $key, array $data)
   {
     $data = json_encode($data);
-    print("KEY: {$key} \n VALUE: {$data} \n\n" );
+    return $this->redis->set(self::DOMAIN_NAMESPACE . $key, $data);
   }
 
+  /**
+   * @param string $key
+   * @return mixed|void
+   */
   public function get(string $key)
   {
     // TODO: Implement get() method.
--- web/modules/custom/webcomposer_domains_configuration_v2/src/Storage/StorageInterface.php
+++ web/modules/custom/webcomposer_domains_configuration_v2/src/Storage/StorageInterface.php
@@ -7,7 +7,20 @@ namespace Drupal\webcomposer_domains_configuration_v2\Storage;
  */
 interface StorageInterface
 {
+  /**
+   * Stores the data using the provided key
+   *
+   * @param string $key
+   * @param array $data
+   * @return bool
+   */
   public function set(string $key, array $data);
 
+  /**
+   * Retrieves the data using the provided key
+   *
+   * @param string $key
+   * @return mixed
+   */
   public function get(string $key);
 }
