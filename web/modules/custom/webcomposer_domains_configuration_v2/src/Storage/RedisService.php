08ef7435faead466a5a7f68157f04a0bd1c3f956
John Patric Rivera <johnpatric.rivera@bayviewtechnology.com>
2020-03-31 15:07:29 +0800

    Create config for redis

--- web/modules/custom/webcomposer_domains_configuration_v2/src/Storage/RedisService.php
+++ web/modules/custom/webcomposer_domains_configuration_v2/src/Storage/RedisService.php
@@ -2,32 +2,18 @@
 
 namespace Drupal\webcomposer_domains_configuration_v2\Storage;
 
+use Drupal\Core\Site\Settings;
 use Predis\Client as Redis;
 
 /**
  * Class RedisService.
  */
-class RedisService implements StorageInterface {
+class RedisService implements StorageInterface
+{
 
-  /**
-   *
-   */
   protected const DOMAIN_NAMESPACE = "domains";
 
   protected const DEFAULT_LANG = "en";
-
-  // TODO: Create a config for this
-  /**
-   * @var array
-   */
-  protected $redisConfig = [
-    'client' => 'tcp://10.5.0.7:6379',
-    'options' => [
-      'parameters' => [
-        'database' => 2
-      ]
-    ]
-  ];
 
   /**
    * @var Redis
@@ -37,14 +23,19 @@ class RedisService implements StorageInterface {
   /**
    * Constructs a new RedisService object.
    */
-  public function __construct() {
-    $this->redis = new Redis($this->redisConfig['client'], $this->redisConfig['options']);
+  public function __construct()
+  {
+    $settings = Settings::get('webcomposer_domains_configuration_v2')['redis'];
+    $this->redis = new Redis(
+      $settings['clients'],
+      $settings['options']
+    );
   }
 
   /** @inheritDoc */
   public function set(string $key, array $data, string $lang = self::DEFAULT_LANG)
   {
-    return $this->redis->hmset(self::DOMAIN_NAMESPACE .":" . $key . ":" . $lang,  $data);
+    return $this->redis->hmset(self::DOMAIN_NAMESPACE . ":" . $key . ":" . $lang, $data);
   }
 
   /** @inheritDoc */
