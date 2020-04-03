d1f8db56a5179ae19d4b06826b30c91f2fd41995
Yunyce de Jesus <Yunyce.DeJesus@bayviewtechnology.com>
2020-04-03 15:57:27 +0800

    [sprint-20-row] msw site

---
+++ web/sites/mobile-msw/modules/custom/contact_tabs/src/ContactTabsEntityTranslationHandler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\contact_tabs;
+
+use Drupal\content_translation\ContentTranslationHandler;
+
+/**
+ * Defines the translation handler for contact_tabs_entity.
+ */
+class ContactTabsEntityTranslationHandler extends ContentTranslationHandler {
+
+  // Override here the needed methods from ContentTranslationHandler.
+
+}
