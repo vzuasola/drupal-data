d435e5f06e29655b4cda519c1b808b8bd5643fa7
ktolentino <kristallynn.tolentino@megasportsworld.com>
2020-04-24 12:07:52 +0800

    Add annotations

--- web/sites/mobile-msw/modules/custom/reset_password/src/Plugin/Webcomposer/Form/ResetPasswordForm.php
+++ web/sites/mobile-msw/modules/custom/reset_password/src/Plugin/Webcomposer/Form/ResetPasswordForm.php
@@ -46,7 +46,35 @@ class ResetPasswordForm extends WebcomposerFormBase implements WebcomposerFormIn
             '#type' => 'textfield',
             '#description' => 'label for new password field placeholder',
             '#default_value' => 'New Password',
+          ],
+          'annotation' => [
+            '#title' => 'Annotation text',
+            '#type' => 'textarea',
+            '#description' => 'field annotation that will be displayed on focus',
+          ],
+          'annotation_weak' => [
+            '#title' => 'Weak Annotation text',
+            '#type' => 'textarea',
+            '#description' => 'Message that will be displayed on lost focus',
+          ],
+          'annotation_average' => [
+            '#title' => 'Average Annotation text',
+            '#type' => 'textarea',
+            '#description' => 'Message that will be displayed on lost focus',
           ]
+        ]
+      ],
+
+      'password_meter' => [
+        'name' => 'Password Meter',
+        'type' => 'markup',
+        'settings' => [
+          'markup' => [
+            '#title' => 'Password meter markup',
+            '#type' => 'textarea',
+            '#description' => 'HTML markup for password meter.',
+            '#default_value' => '<div class="password_meter_wrapper password-meter-hidden grid mb-0"></div>',
+          ],
         ]
       ],
 
@@ -64,7 +92,18 @@ class ResetPasswordForm extends WebcomposerFormBase implements WebcomposerFormIn
             '#type' => 'textfield',
             '#description' => 'label for confirm new password field placeholder',
             '#default_value' => 'Confirm New Password',
-          ]
+          ],
+          'annotation' => [
+            '#title' => 'Annotation text',
+            '#type' => 'textarea',
+            '#description' => 'field annotation that will be displayed on focus',
+          ],
+          'enable' => [
+            '#title' => 'Enable Field',
+            '#type' => 'checkbox',
+            '#description' => 'Check to enable field',
+            '#default_value' => TRUE,
+          ],
         ]
       ],
 
