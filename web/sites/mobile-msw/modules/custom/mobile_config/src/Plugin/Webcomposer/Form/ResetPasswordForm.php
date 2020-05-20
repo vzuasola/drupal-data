49b0fea55cfdb2d7b18d6b0e9c928bb402a58487
ktolentino <kristallynn.tolentino@megasportsworld.com>
2020-05-20 11:42:19 +0800

    Change path

--- web/sites/mobile-msw/modules/custom/mobile_config/src/Plugin/Webcomposer/Form/ResetPasswordForm.php
+++ web/sites/mobile-msw/modules/custom/mobile_config/src/Plugin/Webcomposer/Form/ResetPasswordForm.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Drupal\reset_password\Plugin\Webcomposer\Form;
+namespace Drupal\mobile_config\Plugin\Webcomposer\Form;
 
 use Drupal\webcomposer_form_manager\WebcomposerFormBase;
 use Drupal\webcomposer_form_manager\WebcomposerFormInterface;
