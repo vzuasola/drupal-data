e53dcd9a8cea880de1a424da7e81a6fb9f98c11e
Regie Jose <Regie.Jose@bayviewtechnology.com>
2020-05-28 12:34:40 +0800

    commit profile form config

---
+++ web/sites/nextbet/modules/custom/account_config/src/Form/MyAccountProfileForm.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Drupal\account_profile_config\Form;
+
+use Drupal\webcomposer_config_schema\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * My AccountProfile Form configuration.
+ *
+ * @WebcomposerConfigPlugin(
+ *   id = "profile_form",
+ *   route = {
+ *     "title" = "Profile Form",
+ *     "path" = "/admin/config/my-account/profile",
+ *   },
+ *   menu = {
+ *     "title" = "Profile Form Configuration",
+ *     "description" = "Profile Form COnfiguration",
+ *     "parent" = "account_config.list",
+ *   },
+ * )
+ */
+class MyAccountProfileForm extends FormBase {
+    /**
+     * {@inheritdoc}
+     */
+    protected function getEditableConfigNames() {
+        return ['account_profile_config.general_configuration'];
+    }
+
+    /**
+     * Build the form.
+     *
+     * @inheritdoc
+     */
+    public function form(array $form, FormStateInterface $form_state) {
+        $form['profile'] = [
+            '#type' => 'vertical_tabs',
+        ];
+
+        $form['field_labels_country_mapping'] = [
+            '#type' => 'details',
+            '#title' => 'Country Mapping',
+            '#open' => False,
+            '#group' => 'profile',
+        ];
+
+        $form['field_labels_country_mapping']['country_mapping'] = [
+            '#type' => 'textarea',
+            '#title' => $this->t('Country Mapping'),
+            '#size' => 25,
+            '#required' => TRUE,
+            '#default_value' => $this->get('country_mapping'),
+        ];
+
+        $form['field_labels_country_mapping']['country_code_mapping'] = [
+            '#type' => 'textarea',
+            '#title' => $this->t('Country Code Mapping'),
+            '#size' => 25,
+            '#required' => TRUE,
+            '#default_value' => $this->get('country_code_mapping'),
+        ];
+
+        return $form;
+    }
+}
