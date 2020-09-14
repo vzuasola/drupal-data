16161ef449c0bd92192bda76127346e7cd2b6f46
Vladimir Zuasola <vladimir.zuasola@bayviewtechnology.com>
2020-09-14 16:57:20 +0800

    updated POC lightbox dl app

--- web/sites/mobile-entrypage/modules/custom/mobile_entrypage/src/Form/MobileEntrypageForm.php
+++ web/sites/mobile-entrypage/modules/custom/mobile_entrypage/src/Form/MobileEntrypageForm.php
@@ -231,5 +231,21 @@ class MobileEntrypageForm extends FormBase {
       '#default_value' => $this->get('launch_app_link'),
       '#translatable' => TRUE,
     ];
+
+    $defaultValue = $this->get('mobile_launch_verify_description_select');
+    $form['entrypage_configuration_download_popup']['launch_app']['mobile_launch_verify_description_select'] = [
+      '#type' => 'text_format',
+      '#title' => $this->t('Launch App Verification Description'),
+      '#default_value' => $defaultValue['value'],
+      '#format' => $defaultValue['format'],
+      '#translatable' => true,
+    ];
+
+    $form['entrypage_configuration_download_popup']['launch_app']['launch_app_launch_it'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Link Title - Ok, Got it'),
+      '#default_value' => $this->get('launch_app_launch_it'),
+      '#translatable' => TRUE,
+    ];
   }
 }
