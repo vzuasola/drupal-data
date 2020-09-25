afe1a7ffcf83f2419056f29342f1a66c9f3d4662
Stephanie Aviles <stephanie.aviles@bayviewtechnology.com>
2020-09-25 14:36:46 +0800

    Config to override graphyte keys

--- web/sites/mobile-arcade/modules/custom/arcade/src/Form/ArcadeForm.php
+++ web/sites/mobile-arcade/modules/custom/arcade/src/Form/ArcadeForm.php
@@ -106,6 +106,27 @@ class ArcadeForm extends FormBase {
         '#open' => TRUE,
       ];
 
+      $form['graphyte']['asset'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('API URL'),
+        '#default_value' => $this->get('asset'),
+        '#translatable' => false,
+      ];
+
+      $form['graphyte']['api_key'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('API Key (ClickStream)'),
+        '#default_value' => $this->get('api_key'),
+        '#translatable' => false,
+      ];
+
+      $form['graphyte']['brand_key'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('Brand Key'),
+        '#default_value' => $this->get('brand_key'),
+        '#translatable' => false,
+      ];
+
       $form['graphyte']['enable_clickstream'] = [
         '#type' => 'checkbox',
         '#title' => $this->t('Enable Graphyte Clickstream'),
