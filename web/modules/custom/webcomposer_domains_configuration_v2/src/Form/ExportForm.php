fd0d15a181d187f2aa65e97f968278a39ec38299
John Patric Rivera <johnpatric.rivera@bayviewtechnology.com>
2020-11-04 16:11:14 +0800

    [MDAFABET-232] - Create the export service

--- web/modules/custom/webcomposer_domains_configuration_v2/src/Form/ExportForm.php
+++ web/modules/custom/webcomposer_domains_configuration_v2/src/Form/ExportForm.php
@@ -4,17 +4,28 @@ namespace Drupal\webcomposer_domains_configuration_v2\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\webcomposer_config_schema\Annotation\WebcomposerConfigPlugin;
+use Drupal\webcomposer_domains_configuration_v2\Service\DomainExportService;
 
 class ExportForm extends FormBase {
+  /**
+   *
+   */
   const FORM_ID = 'export_domains_form';
+  /**
+   * @var
+   */
   protected static $instanceId;
+  /**
+   * @var DomainExportService
+   */
+  private $domainExport;
+
 
   /**
    * Constructor.
    */
   public function __construct() {
-
+    $this->domainExport = \Drupal::service('webcomposer_domains_configuration_v2.domain_export');
   }
 
   /**
@@ -30,10 +41,18 @@ class ExportForm extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $form['description'] = [
-      '#type' => 'item',
-      '#title' => $this->t('Export Form'),
-      '#markup' => $this->t('Under Construction'),
+    $form['webcomposer_domain_export'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Export Domains'),
+      '#description' => $this->t('Allows you to export all domain data to an editable spreadsheet file.'),
+    ];
+    $form['webcomposer_domain_export']['label'] = [
+      '#markup' => '<p></p>',
+    ];
+    $form['webcomposer_domain_export']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Export'),
+      '#submit' => [[$this->domainExport, 'execute']],
     ];
 
     return $form;
---
+++ web/modules/custom/webcomposer_domains_configuration_v2/src/Service/DomainExportService.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Drupal\webcomposer_domains_configuration_v2\Service;
+
+use Drupal\webcomposer_domains_configuration_v2\Parser\ExcelParser;
+use Drupal\webcomposer_domains_configuration_v2\Storage\StorageService;
+
+/**
+ * Class DomainExportService.
+ */
+class DomainExportService
+{
+
+  const DEFAULT_LANG = 'en';
+
+  /**
+   * @var ExcelParser
+   */
+  protected $excelParser;
+
+  /**
+   * @var StorageService
+   */
+  protected $storage;
+
+  /**
+   * Constructs a new DomainImportService object.
+   */
+  public function __construct(ExcelParser $excelParser, StorageService $storage)
+  {
+    $this->excelParser = $excelParser;
+    $this->storage = $storage;
+  }
+
+  /**
+   * executes the export process
+   */
+  public function execute()
+  {
+    $tokens = $this->getTokensSheet();
+    $groups = $this->getGroupSheet();
+
+    // Create tokens sheet
+    $this->excelParser->createSheet($tokens, 'tokens');
+    foreach ($groups as $group => $domains) {
+      // Create domains sheet (with domains)
+      $this->excelParser->createSheet($domains, $group);
+    }
+
+    // Invoke excel creation and download.
+    $this->excelParser->save('export.xlsx');
+
+    // Stop script only if headers is set to invoke a download.
+    exit;
+  }
+
+  /**
+   * Retrieves the tokens from storage
+   *
+   * @return array
+   */
+  private function getTokensSheet()
+  {
+    $tokenSheet[] = ['tokens', "Default"];
+    foreach ($this->storage->getTokens() as $token => $default) {
+      $tokenSheet[] = [$token, $default];
+    }
+
+    return $tokenSheet;
+  }
+
+  /**
+   * Retrieves the groups from storage
+   *
+   * @return array
+   */
+  private function getGroupSheet()
+  {
+    $groups = $this->storage->getGroups();
+    $groupSheet = [];
+    $tokens = array_keys($this->storage->getTokens());
+
+    array_unshift($tokens, "domains");
+    foreach ($groups as $group => $domains) {
+      $groupName = str_replace('groups:', '', $group);
+      $groupSheet[$groupName][] = $tokens; // Set the header
+      foreach ($domains as $domain) {
+        $domainRow = array_merge([$domain], array_values($this->getDomainDetails($domain)));
+        $groupSheet[$groupName][] = $domainRow;
+      }
+    }
+
+    return $groupSheet;
+  }
+
+  /**
+   * @param $domain
+   * @param $lang
+   * @return array
+   */
+  private function getDomainDetails($domain, $lang = self::DEFAULT_LANG)
+  {
+    $domainDetails = $this->storage->getDomain($domain, $lang);
+    return $domainDetails;
+  }
+
+}
