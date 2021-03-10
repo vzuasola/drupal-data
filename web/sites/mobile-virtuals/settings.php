d59c3cbe64bb4cb31db6e8cb91a0dc742fb388b9
John Patric Rivera <johnpatric.rivera@bayviewtechnology.com>
2021-03-10 13:23:59 +0800

    Update the views fields of casino and casino gold

--- web/sites/mobile-virtuals/settings.php
+++ web/sites/mobile-virtuals/settings.php
@@ -8,15 +8,3 @@ require $app_root . '/../config/base.settings.php';
  */
 $settings['product'] = 'mobile-entrypage';
 $settings['primary_site_prefix'] = '';
-$databases['default']['default'] = array (
-  'database' => 'mobile_virtuals_2',
-  'username' => 'root',
-  'password' => 'secret',
-  'prefix' => '',
-  'host' => '10.5.0.6',
-  'port' => '3306',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-  'driver' => 'mysql',
-);
-$settings['hash_salt'] = 'ermmPZUJ53qKo0CbMhi0k5tOQWGMH0sM8uhZAL0vuZ1ebURZyV-liwYPaIo99QnPaK-BivOgFQ';
-$settings['install_profile'] = 'fconfig_installer';
