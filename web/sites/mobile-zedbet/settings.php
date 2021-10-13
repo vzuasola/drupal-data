b6da5619cb0311b356877ab3f6e1caad5f412482
Zarko Gavric <zarko.gavric@sportion.net>
2021-10-13 11:35:18 +0200

    Updated settings.php file

--- web/sites/mobile-zedbet/settings.php
+++ web/sites/mobile-zedbet/settings.php
@@ -7,16 +7,3 @@ require $app_root . '/../config/base.settings.php';
  * The product code
  */
 $settings['product'] = 'mobile-zedbet';
-$settings['ck_editor_inline_image_prefix'] = '/en/mobile-zedbet';
-$databases['default']['default'] = array (
-  'database' => 'mobile_zedbet',
-  'username' => 'root',
-  'password' => 'secret',
-  'prefix' => '',
-  'host' => '10.5.0.6',
-  'port' => '3306',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-  'driver' => 'mysql',
-);
-$settings['hash_salt'] = 'nAnOgvRkuq0M3zDHzZmysrxyB6fbl336Kd9u4SkJbOY-csnrb6t2mgVyBU0HMsvF_TZHAGle7w';
-$settings['install_profile'] = 'config_installer';
