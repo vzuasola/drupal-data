ea4417d837121364144bb23b87076ffa26ff59ea
Zarko Gavric <zarko.gavric@sportion.net>
2021-10-13 11:43:58 +0200

    Updated settings.php file

--- web/sites/mobile-zedbet/settings.php
+++ web/sites/mobile-zedbet/settings.php
@@ -7,3 +7,13 @@ require $app_root . '/../config/base.settings.php';
  * The product code
  */
 $settings['product'] = 'mobile-zedbet';
+
+/**
+ * The main URL prefix for this site instance
+ */
+$settings['primary_site_prefix'] = 'mobile-zedbet';
+
+/**
+ * The front end prefix that CKEditor will append for all products
+ */
+$settings['ck_editor_inline_image_prefix'] = '/en/mobile-zedbet';
