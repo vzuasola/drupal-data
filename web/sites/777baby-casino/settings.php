8937e6f991376eef1aef230b4fd2dbf2a0f5d9b7
Lea Priscila Zaragoza <leapriscila.zaragoza@bayviewtechnology.com>
2022-05-03 17:04:57 +0800

    rename module config

--- web/sites/777baby-casino/settings.php
+++ web/sites/777baby-casino/settings.php
@@ -8,4 +8,3 @@ require $app_root . '/../config/base.settings.php';
  */
 $settings['product'] = '777baby-casino';
 $config['system.logging']['error_level'] = 'verbose'; 
-$settings['rebuild_access'] = TRUE;
