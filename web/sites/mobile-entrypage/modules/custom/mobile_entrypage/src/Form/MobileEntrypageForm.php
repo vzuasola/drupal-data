6db3bf3913db0bf1216e0d805fd9a644c5313aac
Joao Vieira <joao.vieira@sportserve.co>
2024-05-28 13:11:14 +0000

    WEBSITE-5284 Get rid of MEPG endpoint for Arion metrics

--- web/sites/mobile-entrypage/modules/custom/mobile_entrypage/src/Form/MobileEntrypageForm.php
+++ web/sites/mobile-entrypage/modules/custom/mobile_entrypage/src/Form/MobileEntrypageForm.php
@@ -42,7 +42,6 @@ class MobileEntrypageForm extends FormBase {
     $this->sectionEntrypageConfigs($form);
     $this->sectionEntrypageFooter($form);
     $this->sectionDownloadLightbox($form);
-    $this->sectionPerformanceMetricsConfigs($form);
     $this->sectionFaqsConfigs($form);
 
     return $form;
@@ -272,22 +271,6 @@ class MobileEntrypageForm extends FormBase {
       '#type' => 'textfield',
       '#title' => $this->t('Launch Game'),
       '#default_value' => $this->get('launch_app_link'),
-      '#translatable' => TRUE,
-    ];
-  }
-
-  private function sectionPerformanceMetricsConfigs(array &$form)
-  {
-    $form['performance_metrics_configuration'] = [
-      '#type' => 'details',
-      '#title' => t('Performance Metrics Configurationn'),
-      '#group' => 'advanced',
-    ];
-
-    $form['performance_metrics_configuration']['metricsEndpoint'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Performance Metrics Endpoint'),
-      '#default_value' => $this->get('metricsEndpoint'),
       '#translatable' => TRUE,
     ];
   }
