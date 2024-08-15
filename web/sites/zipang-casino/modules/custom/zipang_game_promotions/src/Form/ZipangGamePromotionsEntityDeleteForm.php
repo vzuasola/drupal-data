6233d7ae0c5ac3aa8e9804de13d1d1c6de424da9
Lea Zaragoza <leazaragoza@sportserve.co>
2024-08-15 23:06:13 +0800

    PACIFIC-1307 add new game promotions module

---
+++ web/sites/zipang-casino/modules/custom/zipang_game_promotions/src/Form/ZipangGamePromotionsEntityDeleteForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\zipang_game_promotions\Form;
+
+use Drupal\Core\Entity\ContentEntityDeleteForm;
+
+/**
+ * Provides a form for deleting Zipang game promotions entity entities.
+ *
+ * @ingroup zipang_game_promotions
+ */
+class ZipangGamePromotionsEntityDeleteForm extends ContentEntityDeleteForm {
+}
